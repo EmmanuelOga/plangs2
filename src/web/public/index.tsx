87480da55fa5ed3aeb1f5abbf2f7780bc741f82b
Emmanuel Oga <contact@github.emmanueloga.com>
2024-06-16 01:48:35 -0700

    add some more interactivity to graph rendering

--- src/web/public/index.tsx
+++ src/web/public/index.tsx
@@ -7,34 +7,131 @@
 // // root.render(<h1>Hello, world</h1>);
 
 import Graph from "graphology";
-import Sigma from "sigma";
+import { cropToLargestConnectedComponent } from "graphology-components";
 import forceAtlas2 from "graphology-layout-forceatlas2";
-import { random } from "graphology-layout";
+import circular from "graphology-layout/circular";
+import Sigma from "sigma";
+import { EdgeDisplayData, NodeDisplayData } from "sigma/types";
 
 async function render() {
+  const data = await fetch("http://localhost:3000/data.json").then((res) =>
+    res.json()
+  );
+
   const graph = new Graph();
-  const data = await fetch("http://localhost:3000/data.json").then((res) => res.json());
   graph.import(data);
-  console.log(data);
+  cropToLargestConnectedComponent(graph);
 
-  random.assign(graph);
-  forceAtlas2.assign(graph, { iterations: 50 });
+  // Assign node size by degree.
+  const degrees = graph.nodes().map((node) => graph.degree(node));
+  const minDegree = Math.min(...degrees);
+  const maxDegree = Math.max(...degrees);
 
-  graph.forEachNode((node, attr) => {
-    attr.size = 20;
+  graph.forEachNode((node) => {
+    const degree = graph.degree(node);
+    const level = (degree - minDegree) / (maxDegree - minDegree);
+    const size = 3 + 25 * Math.pow(2, 3 * level - 3);
+    graph.setNodeAttribute(node, "size", size);
   });
 
-  graph.forEachEdge((edge, attr) => {
-    attr.size = 10;
-  });
+  circular.assign(graph);
+  const settings = forceAtlas2.inferSettings(graph);
+  forceAtlas2.assign(graph, { settings, iterations: 600 });
 
   const container = document.getElementById("app") as HTMLElement;
-
-  const renderer = new Sigma(graph, container, {
-    renderEdgeLabels: true
-  });
+  const renderer = new Sigma(graph, container, { renderEdgeLabels: true });
 
   renderer.refresh();
+
+  setupHover(graph, renderer);
 }
 
 render();
+
+function setupHover(graph: Graph, renderer: Sigma) {
+  // Type and declare internal state:
+  interface State {
+    hoveredNode?: string;
+    selectedNode?: string;
+    hoveredNeighbors?: Set<string>;
+  }
+
+  const state: State = {};
+
+  function setHoveredNode(node?: string) {
+    if (node) {
+      state.hoveredNode = node;
+      state.hoveredNeighbors = new Set(graph.neighbors(node));
+    }
+
+    // Compute the partial that we need to re-render to optimize the refresh
+    const nodes = graph.filterNodes(
+      (n) => n !== state.hoveredNode && !state.hoveredNeighbors?.has(n)
+    );
+    const nodesIndex = new Set(nodes);
+    const edges = graph.filterEdges((e) =>
+      graph.extremities(e).some((n) => nodesIndex.has(n))
+    );
+
+    if (!node) {
+      state.hoveredNode = undefined;
+      state.hoveredNeighbors = undefined;
+    }
+
+    // Refresh rendering
+    renderer.refresh({
+      partialGraph: {
+        nodes,
+        edges,
+      },
+      // We don't touch the graph data so we can skip its reindexation
+      skipIndexation: true,
+    });
+  }
+
+  // Bind graph interactions:
+  renderer.on("enterNode", ({ node }) => {
+    setHoveredNode(node);
+  });
+  renderer.on("leaveNode", () => {
+    setHoveredNode(undefined);
+  });
+
+  // Render nodes accordingly to the internal state:
+  // 1. If a node is selected, it is highlighted
+  // 2. If there is query, all non-matching nodes are greyed
+  // 3. If there is a hovered node, all non-neighbor nodes are greyed
+  renderer.setSetting("nodeReducer", (node, data) => {
+    const res: Partial<NodeDisplayData> = { ...data };
+
+    if (
+      state.hoveredNeighbors &&
+      !state.hoveredNeighbors.has(node) &&
+      state.hoveredNode !== node
+    ) {
+      res.label = "";
+      res.color = "#f6f6f6";
+    }
+
+    if (state.selectedNode === node) {
+      res.highlighted = true;
+    }
+
+    return res;
+  });
+
+  // Render edges accordingly to the internal state:
+  // 1. If a node is hovered, the edge is hidden if it is not connected to the
+  //    node
+  // 2. If there is a query, the edge is only visible if it connects two
+  //    suggestions
+  renderer.setSetting("edgeReducer", (edge, data) => {
+    const res: Partial<EdgeDisplayData> = { ...data };
+
+    if (state.hoveredNode && !graph.hasExtremity(edge, state.hoveredNode)) {
+      res.hidden = true;
+    }
+
+    return res;
+  });
+}
