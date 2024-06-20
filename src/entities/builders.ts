5d5dbe0c46f9f56e00452ddce094d404fbbf692e
Emmanuel Oga <contact@github.emmanueloga.com>
2024-06-19 22:34:29 -0700

    adding some builders

--- src/entities/builders.ts
+++ src/entities/builders.ts
@@ -2,35 +2,41 @@ import type { PlangsGraph } from "./plangs_graph";
 import type { Image, Link, Release, T_Id_V_Paradigm, V_Plang } from "./schemas";
 
 export class PlangsBuilder {
-	constructor(readonly g: PlangsGraph) {}
+	constructor(readonly g: PlangsGraph) { }
 
-	addDialects(pl: Partial<V_Plang>, vids: string[]) {}
+	addDialects(pl: Partial<V_Plang>, vids: string[]) { }
 
-	addExtensions(pl: Partial<V_Plang>, arg0: string[]) {}
+	addExtensions(pl: Partial<V_Plang>, arg0: string[]) { }
 
-	addImages(pl: Partial<V_Plang>, images: Image[]) {}
+	addImages(pl: Partial<V_Plang>, images: Image[]) { }
 
-	addImplementations(pl: Partial<V_Plang>, vids: string[]) {}
+	addImplementations(pl: Partial<V_Plang>, vids: string[]) { }
 
-	addInfluenced(pl: Partial<V_Plang>, vids: string[]) {}
+	addInfluenced(pl: Partial<V_Plang>, vids: string[]) { }
 
-	addInfluences(pl: Partial<V_Plang>, vids: string[]) {}
+	addInfluences(pl: Partial<V_Plang>, vids: string[]) { }
 
-	addLicenses(pl: Partial<V_Plang>, vids: string[]) {}
+	addLicenses(pl: Partial<V_Plang>, vids: string[]) { }
 
-	addParadigms(pl: Partial<V_Plang>, arg0: string[]) {}
+	addParadigms(pl: Partial<V_Plang>, arg0: string[]) { }
 
-	addPeople(pl: Partial<V_Plang>, vids: string[][]) {}
+	addPeople(pl: Partial<V_Plang>, vids: string[][]) { }
 
-	addPlatforms(pl: Partial<V_Plang>, vids: string[]) {}
+	addPlatforms(pl: Partial<V_Plang>, vids: string[]) { }
 
-	addReferences(pl: Partial<V_Plang>, references: Record<string, Link[]>) {}
+	addReferences(pl: Partial<V_Plang>, references: Record<string, Link[]>) { }
 
-	addReleases(pl: Partial<V_Plang>, releases: Release[]) {}
+	addReleases(pl: Partial<V_Plang>, releases: Release[]) { }
 
-	addScoping(pl: Partial<V_Plang>, vids: string[]) {}
+	addScoping(pl: Partial<V_Plang>, vids: string[]) { }
 
-	addTypeSystems(pl: Partial<V_Plang>, vids: string[]) {}
+	addTypeSystems(pl: Partial<V_Plang>, vids: string[]) { }
 
-	addWebsites(pl: Partial<V_Plang>, websites: Link[]) {}
+	addWebsites(pl: Partial<V_Plang>, websites: Link[]) { }
 }
+
+export class LicenseBuilder { constructor(readonly g: PlangsGraph) { } }
+export class ParadigmBuilder { constructor(readonly g: PlangsGraph) { } }
+export class PersonBuilder { constructor(readonly g: PlangsGraph) { } }
+export class PlatformBuilder { constructor(readonly g: PlangsGraph) { } }
+export class TypeSysBuilder { constructor(readonly g: PlangsGraph) { } }
--- src/entities/plangs_graph.ts
+++ src/entities/plangs_graph.ts
@@ -1,22 +1,6 @@
 import { Graph } from "../graph/base";
-import { PlangsBuilder } from "./builders";
-import type {
-	E_Empty,
-	E_People,
-	Release,
-	T_Id_V_License,
-	T_Id_V_Paradigm,
-	T_Id_V_Person,
-	T_Id_V_Plang,
-	T_Id_V_Platform,
-	T_Id_V_TypeSystem,
-	V_License,
-	V_Paradigm,
-	V_Person,
-	V_Plang,
-	V_Platform,
-	V_TypeSystem,
-} from "./schemas";
+import { LicenseBuilder, ParadigmBuilder, PersonBuilder, PlangsBuilder, PlatformBuilder, TypeSysBuilder } from "./builders";
+import type { E_Empty, E_People, T_Id_V_License, T_Id_V_Paradigm, T_Id_V_Person, T_Id_V_Plang, T_Id_V_Platform, T_Id_V_TypeSystem, V_License, V_Paradigm, V_Person, V_Plang, V_Platform, V_TypeSystem, } from "./schemas";
 
 /**
  * Collection of related edge and vertex tables.
@@ -26,64 +10,29 @@ import type {
  */
 export class PlangsBase extends Graph {
 	// Vertex tables.
-	readonly v_license = this.v_table<Partial<V_License>, T_Id_V_License>(
-		"license",
-	);
-	readonly v_paradigm = this.v_table<Partial<V_Paradigm>, T_Id_V_Paradigm>(
-		"para",
-	);
+	readonly v_license = this.v_table<Partial<V_License>, T_Id_V_License>( "license");
+	readonly v_paradigm = this.v_table<Partial<V_Paradigm>, T_Id_V_Paradigm>( "para");
 	readonly v_person = this.v_table<Partial<V_Person>, T_Id_V_Person>("person");
 	readonly v_plang = this.v_table<Partial<V_Plang>, T_Id_V_Plang>("pl");
-	readonly v_platform = this.v_table<Partial<V_Platform>, T_Id_V_Platform>(
-		"platf",
-	);
-	readonly v_tsystem = this.v_table<Partial<V_TypeSystem>, T_Id_V_TypeSystem>(
-		"tsys",
-	);
+	readonly v_platform = this.v_table<Partial<V_Platform>, T_Id_V_Platform>( "platf");
+	readonly v_tsystem = this.v_table<Partial<V_TypeSystem>, T_Id_V_TypeSystem>( "tsys");
 
 	// Edge Tables.
-	readonly e_dialect_of = this.e_table<E_Empty, T_Id_V_Plang, T_Id_V_Plang>(
-		"dialect-of",
-		this.v_plang,
-		this.v_plang,
-	);
-	readonly e_has_license = this.e_table<E_Empty, T_Id_V_Plang, T_Id_V_License>(
-		"has-license",
-		this.v_plang,
-		this.v_license,
-	);
-	readonly e_implements = this.e_table<E_Empty, T_Id_V_Plang, T_Id_V_Plang>(
-		"implements",
-		this.v_plang,
-		this.v_plang,
-	);
-	readonly e_l_influenced_l = this.e_table<E_Empty, T_Id_V_Plang, T_Id_V_Plang>(
-		"influenced",
-		this.v_plang,
-		this.v_plang,
-	);
-	readonly e_person_plang_role = this.e_table<
-		E_People,
-		T_Id_V_Person,
-		T_Id_V_Plang
-	>("had-role", this.v_person, this.v_plang);
-	readonly e_plang_para = this.e_table<E_Empty, T_Id_V_Plang, T_Id_V_Paradigm>(
-		"paradigm",
-		this.v_plang,
-		this.v_paradigm,
-	);
-	readonly e_plang_tsys = this.e_table<
-		E_Empty,
-		T_Id_V_Plang,
-		T_Id_V_TypeSystem
-	>("type-system", this.v_plang, this.v_tsystem);
-	readonly e_supports_platf = this.e_table<
-		E_Empty,
-		T_Id_V_Plang,
-		T_Id_V_Platform
-	>("supports-platf", this.v_plang, this.v_platform);
+	readonly e_dialect_of = this.e_table<E_Empty, T_Id_V_Plang, T_Id_V_Plang>( "dialect-of", this.v_plang, this.v_plang);
+	readonly e_has_license = this.e_table<E_Empty, T_Id_V_Plang, T_Id_V_License>( "has-license", this.v_plang, this.v_license);
+	readonly e_implements = this.e_table<E_Empty, T_Id_V_Plang, T_Id_V_Plang>( "implements", this.v_plang, this.v_plang);
+	readonly e_l_influenced_l = this.e_table<E_Empty, T_Id_V_Plang, T_Id_V_Plang>( "influenced", this.v_plang, this.v_plang);
+	readonly e_person_plang_role = this.e_table< E_People, T_Id_V_Person, T_Id_V_Plang >("had-role", this.v_person, this.v_plang);
+	readonly e_plang_para = this.e_table<E_Empty, T_Id_V_Plang, T_Id_V_Paradigm>( "paradigm", this.v_plang, this.v_paradigm);
+	readonly e_plang_tsys = this.e_table< E_Empty, T_Id_V_Plang, T_Id_V_TypeSystem >("type-system", this.v_plang, this.v_tsystem);
+	readonly e_supports_platf = this.e_table< E_Empty, T_Id_V_Plang, T_Id_V_Platform >("supports-platf", this.v_plang, this.v_platform);
 }
 
 export class PlangsGraph extends PlangsBase {
+	readonly licenseBuilder = new LicenseBuilder(this);
+	readonly paradigmBuilder = new ParadigmBuilder(this);
+	readonly personBuilder = new PersonBuilder(this);
 	readonly plangBuilder = new PlangsBuilder(this);
+	readonly platformBuilder = new PlatformBuilder(this);
+	readonly typeSysBuilder = new TypeSysBuilder(this);
 }
