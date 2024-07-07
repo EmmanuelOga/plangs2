8bdd638f857ad88aa02edae7d9647dbe33fcbced
Emmanuel Oga <contact@github.emmanueloga.com>
2024-07-06 22:07:32 -0700

    preparing for filtering languages by name

---
+++ src/frontend/shared/state/search.ts
@@ -0,0 +1,9 @@
+import { Wade } from "../../vendor/Wade/Wade";
+
+type SearchIndex = {
+  search: (term: string) => string[];
+};
+
+function searchIndex(values: [string, string]) {
+  //   const plIdx = Wade([...pg.v_plang].map(([vid, pl]) => pl.name));
+}
