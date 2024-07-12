83d5e3ecf68876e2ab99042de6ee0dfc0fdd87c9
Emmanuel Oga <contact@github.emmanueloga.com>
2024-07-11 18:52:26 -0700

    input-compl boilerplate

--- packages/frontend/src/input-compl/input_compl.tsx
+++ packages/frontend/src/input-compl/input_compl.tsx
@@ -1,7 +1,14 @@
-export function InputCompl() {
-  return (
-    <div>
-      <h1>Soon this will be an Input.</h1>
-    </div>
-  );
+import register from "preact-custom-element";
+
+export type InputComplProps = {
+  complData: [string, string][];
 }
+
+export function InputCompl({ complData }: InputComplProps) {
+  return <div>Input: {JSON.stringify(complData)}</div>;
+}
+
+/** Cast the DOM element to this type to access the custom API. */
+export type InputComplWebComponent = HTMLInputElement & InputComplProps;
+
+register(InputCompl, "input-compl", ["complData"], { shadow: false });
