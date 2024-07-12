1fa9cef0952be392f520671fc3422db9c66945b2
Emmanuel Oga <contact@github.emmanueloga.com>
2024-07-11 19:51:49 -0700

    add css style boilerplate

--- packages/frontend/sandbox/sandbox.tsx
+++ packages/frontend/sandbox/sandbox.tsx
@@ -5,9 +5,6 @@ import type { InputComplWebComponent } from "../src/input-compl/input_compl";
 const c = document.querySelector("input-compl") as InputComplWebComponent;
 if (!c) throw new Error("Component not found");
 
-c.complData = [
-  ["1", "hello"],
-  ["2", "world"],
-  ["3", "bye, bye"],
-  ["4", "world"],
-];
+const words = ["car", "house", "dog", "cat", "mouse", "elephant", "lion", "tiger", "bear", "wolf", "fox", "rabbit"];
+
+c.complData = words.map((w, i) => [`${i}`, w]);
--- packages/frontend/src/input-compl/input_compl.tsx
+++ packages/frontend/src/input-compl/input_compl.tsx
@@ -1,14 +1,31 @@
 import register from "preact-custom-element";
 
-export type InputComplProps = {
-  complData: [string, string][];
-}
+import "./input_compl.css";
 
-export function InputCompl({ complData }: InputComplProps) {
-  return <div>Input: {JSON.stringify(complData)}</div>;
+export type InputComplProps = {
+  name: string;
+
+  /** Array of [data, label] elements. */
+  complData: [string, string][];
+
+  /** Setting this forces a render, without having to replace other data. */
+  version: number;
+};
+
+export function InputCompl({ name, complData }: InputComplProps) {
+  return (
+    <>
+      <input type="search" name={name} />
+      <div class="list">
+        {complData?.map(([data, label]) => (
+          <div class="item" key={data}>{label}</div>
+        ))}
+      </div>
+    </>
+  );
 }
 
 /** Cast the DOM element to this type to access the custom API. */
 export type InputComplWebComponent = HTMLInputElement & InputComplProps;
 
-register(InputCompl, "input-compl", ["complData"], { shadow: false });
+register(InputCompl, "input-compl", ["name", "complData", "version"], { shadow: false });
