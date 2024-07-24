cb5fdbe118fbb94fe477e9ee8e160e6a35858198
Emmanuel Oga <contact@github.emmanueloga.com>
2024-07-24 15:31:06 -0700

    add licensesg

--- packages/frontend/src/plang-info/plang-info.tsx
+++ packages/frontend/src/plang-info/plang-info.tsx
@@ -2,7 +2,7 @@ import type { ComponentChildren, JSX, Ref } from "preact";
 import { useEffect, useRef, useState } from "preact/hooks";
 
 import type { PlangsGraph } from "@plangs/plangs";
-import type { VID_Person, VID_Plang, VID_Platform, VID_TypeSystem, V_Person, V_Platform } from "@plangs/plangs/schema";
+import type { VID_License, VID_Person, VID_Plang, VID_Platform, VID_TypeSystem, V_Person, V_Platform } from "@plangs/plangs/schema";
 import { customEvent, on } from "../utils";
 
 export const TAG_NAME = "plang-info";
@@ -26,11 +26,9 @@ export function PlangInfo({ vid }: PlangInfoProps) {
       const viewportHeight = window.innerHeight;
 
       if (height > viewportHeight) {
-        console.log("stick to bottom");
         div.classList.remove("stick-to-top");
         div.classList.add("stick-to-bottom");
       } else {
-        console.log("stick to top");
         div.classList.add("stick-to-top");
         div.classList.remove("stick-to-bottom");
       }
@@ -56,6 +54,7 @@ export function PlangInfo({ vid }: PlangInfoProps) {
       const people = collect(pg.e_person_plang.adjacentTo(vid), (vid) => pg.v_person.get(vid as VID_Person));
       const platforms = collect(pg.e_supports_platf.adjacentFrom(vid), (vid) => pg.v_platform.get(vid as VID_Platform));
       const tsys = collect(pg.e_plang_tsys.adjacentFrom(vid), (vid) => pg.v_tsystem.get(vid as VID_TypeSystem));
+      const licenses = collect(pg.e_has_license.adjacentFrom(vid), (vid) => pg.v_license.get(vid as VID_License));
 
       const influencedBy = collect(pg.e_l_influenced_l.adjacentTo(vid), (vid) => pg.v_plang.get(vid as VID_Plang));
       const influenced = collect(pg.e_l_influenced_l.adjacentFrom(vid), (vid) => pg.v_plang.get(vid as VID_Plang));
@@ -75,37 +74,60 @@ export function PlangInfo({ vid }: PlangInfoProps) {
         );
       }
 
-      function Pill({ vid, name, cssClasses }: { vid: string; name: string; cssClasses?: string }) {
+      function Pill({ vid, name, kind }: { vid: string; kind: string; name: string }) {
+        const cssClasses = kind === "pl" ? "pill pl-pill" : "pill";
         return (
-          <div class={cssClasses ?? "pill"} key={vid} data-vid={vid}>
+          <div class={cssClasses} key={vid} data-vid={vid} data-kind={kind}>
             {name}
           </div>
         );
       }
-
-      const PlPill = (props: Parameters<typeof Pill>[0]) => Pill({ ...props, cssClasses: "pill pl-pill" });
 
       content = (
         <>
           <h2>{pl.name}</h2>
           <dl>
             {pl.description && <Entry title="Description">{pl.description}</Entry>}
-            {people.length > 0 && <Entry title="People">{people.map((data) => Pill(data))}</Entry>}
-            {pl.scoping && <Entry title="Scoping">{pl.scoping.map((data) => Pill({ vid: data, name: data }))}</Entry>}
-            {pl.extensions && (
-              <Entry title="Extensions">{pl.extensions.map((data) => Pill({ vid: data, name: data }))}</Entry>
+            {people.length > 0 && (
+              <Entry title="People">{people.map((data) => Pill({ ...data, kind: "person" }))}</Entry>
             )}
-            {tsys.length > 0 && <Entry title="Type Systems">{tsys.map((data) => Pill(data))}</Entry>}
-            {platforms.length > 0 && <Entry title="Platforms">{platforms.map((data) => Pill(data))}</Entry>}
-
-            {influencedBy.length > 0 && <Entry title="Influenced By">{influencedBy.map((data) => PlPill(data))}</Entry>}
-            {influenced.length > 0 && <Entry title="Influenced">{influenced.map((data) => PlPill(data))}</Entry>}
-
-            {dialectOf.length > 0 && <Entry title="Dialect Of">{dialectOf.map((data) => PlPill(data))}</Entry>}
-            {children.length > 0 && <Entry title="Implemented By">{children.map((data) => PlPill(data))}</Entry>}
-
-            {impls.length > 0 && <Entry title="Implementations">{impls.map((data) => PlPill(data))}</Entry>}
-            {standards.length > 0 && <Entry title="Standard For">{standards.map((data) => PlPill(data))}</Entry>}
+            {pl.scoping && (
+              <Entry title="Scoping">
+                {pl.scoping.map((data) => Pill({ vid: data, name: data, kind: "scoping" }))}
+              </Entry>
+            )}
+            {pl.extensions && (
+              <Entry title="Extensions">
+                {pl.extensions.map((data) => Pill({ vid: data, name: data, kind: "ext" }))}
+              </Entry>
+            )}
+            {tsys.length > 0 && (
+              <Entry title="Type Systems">{tsys.map((data) => Pill({ ...data, kind: "tsys" }))}</Entry>
+            )}
+            {platforms.length > 0 && (
+              <Entry title="Platforms">{platforms.map((data) => Pill({ ...data, kind: "platf" }))}</Entry>
+            )}
+            {influencedBy.length > 0 && (
+              <Entry title="Influenced By">{influencedBy.map((data) => Pill({ ...data, kind: "pl" }))}</Entry>
+            )}
+            {influenced.length > 0 && (
+              <Entry title="Influenced">{influenced.map((data) => Pill({ ...data, kind: "pl" }))}</Entry>
+            )}
+            {dialectOf.length > 0 && (
+              <Entry title="Dialect Of">{dialectOf.map((data) => Pill({ ...data, kind: "pl" }))}</Entry>
+            )}
+            {children.length > 0 && (
+              <Entry title="Implemented By">{children.map((data) => Pill({ ...data, kind: "pl" }))}</Entry>
+            )}
+            {impls.length > 0 && (
+              <Entry title="Implementations">{impls.map((data) => Pill({ ...data, kind: "pl" }))}</Entry>
+            )}
+            {standards.length > 0 && (
+              <Entry title="Standard For">{standards.map((data) => Pill({ ...data, kind: "pl" }))}</Entry>
+            )}
+            {licenses.length > 0 && (
+              <Entry title="Licenses">{licenses.map((data) => Pill({ ...data, kind: "lic" }))}</Entry>
+            )}
 
             {/*
               <dt>Misc</dt>
