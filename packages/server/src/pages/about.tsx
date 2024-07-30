ee92dfe7d742477aaf574392162c9353babfd549
Emmanuel Oga <contact@github.emmanueloga.com>
2024-07-30 02:26:06 -0700

    updates to about page

--- packages/server/src/pages/about.tsx
+++ packages/server/src/pages/about.tsx
@@ -1,31 +1,36 @@
 export function About() {
-  const text = [
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    "Ut enim ad minim veniam, quis nostrud exercitation.",
-    "Ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    "Duis aute irure dolor in reprehenderit in voluptate.",
-    "Velit esse cillum dolore eu fugiat nulla pariatur.",
-    "Excepteur sint occaecat cupidatat non proident, sunt in.",
-    "Culpa qui officia deserunt mollit anim id est laborum.",
-  ];
-
-  function para(n: number, j = 0) {
-    return Array.from({ length: n }, (_, i) => {
-      return text[(i + j) % text.length];
-    }).join(" ");
-  }
-
   return (
     <>
       <div />
       <article id="about" class="common-content">
-        {Array.from({ length: 5 }, (_, i) => (
-          <>
-            <h2>{para(1, i)}</h2>
-            <p>{para(20 + Math.abs(Math.sin(i) * 20))}</p>
-          </>
-        ))}
+        <h2>What is Plangs?</h2>
+        <p>
+          Plangs is a website dedicated to aggregating and curating information about a wide variety of programming
+          languages.
+        </p>
+        <h2>Our Data Source</h2>
+        <p>
+          The initial data for Plangs was scraped from Wikipedia infoboxes on programming languages. While Wikipedia
+          offers a rich repository of information, not all programming languages have dedicated pages.
+        </p>
+        <h2>Community Contribution</h2>
+        <p>
+          If you find any mistakes or want to submit new information, please feel free to open a pull request on our
+          GitHub repository. We welcome contributions from the community to help us keep our data up-to-date and
+          accurate.
+        </p>
+        <h2>Static Website and Baked Data</h2>
+        <p>
+          Plangs is built as a static website. All the data on our site is available as a JSON file, following the
+          "baked data" pattern inspired by{" "}
+          <a href="https://simonwillison.net/2021/Jul/28/baked-data/">Simon Willison's Baked Data Pattern</a>.
+        </p>
+        <h2>License</h2>
+        <p>
+          Plangs © 2024 by <a href="https://emmanueloga.com">Emmanuel Oga</a> is licensed under
+          <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0 </a>.
+        </p>
+        <h3>Thank you for your visit!</h3>
       </article>
       <div />
     </>
