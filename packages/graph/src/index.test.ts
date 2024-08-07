1f6e845a80eba57a5f93d879b4589866c2547f8e
Emmanuel Oga <contact@github.emmanueloga.com>
2024-08-06 19:15:33 -0700

    new edge mapper

--- packages/graph/src/index.test.ts
+++ packages/graph/src/index.test.ts
@@ -1,12 +1,15 @@
 /* Example vertex data interfaces: model a blog. */
 import { expect, test } from "bun:test";
-import { Edge, Vertex, VertexMap } from ".";
+import { Edge, EdgeMap, Vertex, VertexMap } from ".";
 
 /** Example: define a graph as a set of vertex and edge maps. */
 class Graph {
   v_person = new VertexMap<VPerson>((id) => new VPerson(this, id));
   v_post = new VertexMap<VPost>((id) => new VPost(this, id));
   v_tag = new VertexMap<VTag>((id) => new VTag(this, id));
+
+  e_person_post = new EdgeMap<EPersonPost>((from, to) => new EPersonPost("person-post", from, to));
+  e_post_tag = new EdgeMap<EPostTag>((from, to) => new EPostTag("post-tag", from, to));
 }
 
 /** Example: allow access to other graph elements from every vertex wrapper. */
@@ -39,13 +42,42 @@ class EPersonPost extends Edge<{
   from: VPerson["id"];
   to: VPost["id"];
   role: "author" | "editor";
-}> {
-}
+}> {}
 
-test("serializes a vertex", () => {
+class EPostTag extends Edge<{
+  type: "post-tag";
+  from: VPost["id"];
+  to: VTag["id"];
+}> {}
+
+test("adjacent vertices", () => {
   const g = new Graph();
 
-  const postData: Partial<VPost["data"]> = { title: "Hello", content: "World" };
+  const person1 = g.v_person.get("person+1");
+  const person2 = g.v_person.get("person+2");
+
+  const post1 = g.v_post.get("post+1");
+  const post2 = g.v_post.get("post+2");
+  const post3 = g.v_post.get("post+3");
+
+  const tag = g.v_tag.get("tag+1");
+
+  const e1 = g.e_person_post.connect(person1.id, post1.id).merge({ role: "author" });
+  const e2 = g.e_person_post.connect(person2.id, post2.id).merge({ role: "author" });
+  const e3 = g.e_person_post.connect(person2.id, post3.id).merge({ role: "author" });
+  const e4 = g.e_post_tag.connect(post2.id, tag.id);
+
+  expect(g.e_post_tag.adjFrom(post1.id)).toBeEmpty();
+  expect(g.e_post_tag.adjFrom(post2.id)).toEqual(new Map([[tag.id, e4]]));
+  expect(g.e_post_tag.adjFrom(post3.id)).toBeEmpty();
+
+  expect(g.e_post_tag.adjTo(tag.id)).toEqual(new Map([[post2.id, e4]]));
+});
+
+test("serializing a vertex", () => {
+  const g = new Graph();
+
+  const postData: VPost["data"] = { title: "Hello", content: "World" };
 
   const person = g.v_person.get("person+1").merge({ name: "Alice" });
   const post = g.v_post.get("post+1").merge(postData);
@@ -58,6 +90,18 @@ test("serializes a vertex", () => {
   expect(rt(tag)).toEqual({ id: "tag+1" });
 });
 
-const e = new EPersonPost("person-post", "person+1", "post+1");
+test("serializing an edge", () => {
+  const g = new Graph();
 
-console.log(e);
+  const person = g.v_person.get("person+1");
+  const post = g.v_post.get("post+1");
+  const tag = g.v_tag.get("tag+1");
+
+  const e1 = g.e_person_post.connect(person.id, post.id).merge({ role: "author" });
+  const e2 = g.e_post_tag.connect(post.id, tag.id);
+
+  const rt = (o: Edge<any>) => JSON.parse(JSON.stringify(o.data));
+
+  expect(rt(e1)).toEqual({ type: "person-post", from: "person+1", to: "post+1", role: "author" });
+  expect(rt(e2)).toEqual({ type: "post-tag", from: "post+1", to: "tag+1" });
+});
--- packages/graph/src/index.ts
+++ packages/graph/src/index.ts
@@ -1,7 +1,8 @@
-export abstract class Vertex<T_Data extends { id: T }, T = T_Data["id"]> {
+/** Vertex wrapper. */
+export abstract class Vertex<T_Data extends { id: T_Id }, T_Id = T_Data["id"]> {
   readonly data: Partial<T_Data> = {};
 
-  constructor(public readonly id: T) {
+  constructor(public readonly id: T_Id) {
     this.data.id = this.id;
   }
 
@@ -11,8 +12,11 @@ export abstract class Vertex<T_Data extends { id: T }, T = T_Data["id"]> {
   }
 }
 
-// biome-ignore lint/suspicious/noExplicitAny: The vertex types can be any vertex type.
-export class VertexMap<V extends Vertex<any>> {
+/** Extracts the type param from Vertex */
+export type VertexParam<T> = T extends Vertex<infer T_Data> ? T_Data : never;
+
+/** Stores vertices by vertex id. */
+export class VertexMap<V extends Vertex<T_Data>, T_Data extends { id: T_Data["id"] } = VertexParam<V>> {
   readonly #map = new Map<V["id"], V>();
 
   constructor(private ctor: (id: V["id"]) => V) {}
@@ -30,6 +34,7 @@ export class VertexMap<V extends Vertex<any>> {
   }
 }
 
+/** Edge wrapper. */
 export abstract class Edge<
   T_Data extends { type: T_Type; from: T_From; to: T_To },
   T_Type = T_Data["type"],
@@ -54,7 +59,61 @@ export abstract class Edge<
   }
 }
 
-export class EdgeMap<E extends Edge<any>> {
-  readonly #adjTo = new Map<E["to"], Set<E>>();
-  readonly #adjFrom = new Map<E["from"], Set<E>>();
+/** Extracts the type param from Edge. */
+type EdgeParam<T> = T extends Edge<infer T> ? T : never;
+
+/** Stores edges by the compound keys (from, to) and (to, from). */
+export class EdgeMap<E extends Edge<T>, T extends { type: T["type"]; from: T["from"]; to: T["to"] } = EdgeParam<E>> {
+  readonly #adjFrom = new Map2<T["from"], T["to"], E>();
+  readonly #adjTo = new Map2<T["to"], T["from"], E>();
+
+  constructor(private ctor: (from: T["from"], to: T["to"]) => E) {}
+
+  connect(from: T["from"], to: T["to"]): E {
+    let e = this.#adjFrom.get(from, to);
+    if (e) return e;
+    e = this.ctor(from, to);
+    this.#adjFrom.set(from, to, e);
+    this.#adjTo.set(to, from, e);
+    return e;
+  }
+
+  adjFrom(from: T["from"]): Map<T["to"], E> {
+    return this.#adjFrom.getMap(from) ?? new Map();
+  }
+
+  adjTo(to: T["to"]): Map<T["from"], E> {
+    return this.#adjFrom.getMap(to) ?? new Map();
+  }
+}
+
+/** Two dimensional Map. */
+class Map2<K1, K2, V> {
+  #map = new Map<K1, Map<K2, V>>();
+
+  set(k1: K1, k2: K2, v: V): this {
+    let m2 = this.#map.get(k1);
+    if (!m2) {
+      m2 = new Map();
+      this.#map.set(k1, m2);
+    }
+    m2.set(k2, v);
+    return this;
+  }
+
+  get(k1: K1, k2: K2): V | undefined {
+    const m2 = this.#map.get(k1);
+    if (!m2) return undefined;
+    return m2.get(k2);
+  }
+
+  getMap(k1: K1): Map<K2, V> | undefined {
+    return this.#map.get(k1);
+  }
+
+  has(k1: K1, k2: K2): boolean {
+    const m2 = this.#map.get(k1);
+    if (!m2) return false;
+    return m2.has(k2);
+  }
 }
