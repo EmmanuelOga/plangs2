51df04eeae6b41ac8911e04cea3cf837fb2d384a
Emmanuel Oga <contact@github.emmanueloga.com>
2024-10-09 08:34:15 -0700

    remove some borders

--- packages/server/src/pages/browse.tsx
+++ packages/server/src/pages/browse.tsx
@@ -17,9 +17,9 @@ export function Browse({ pg }: { pg: PlangsGraph }) {
       description="Discover https://plangs.page faceted search engine to find your next favorite programming language."
       overflow="overflow-auto">
       <div class={tw("h-full w-full flex-1", "flex flex-col lg:flex-row", "gap-4", "overflow-auto")}>
-        <div class={tw("flex-1", "flex flex-col", "gap-2", "overflow-auto")}>
-          <PlFilters class={tw(SCROLL, "z-10", "px-4 py-2", "max-h-[40%]", "border-1 border-primary border-t-0")} />
-          <PlGrid pg={pg} class={tw(SCROLL, "p-4", "flex-1", "pl-bg-g", "border-1 border-primary")} />
+        <div class={tw("flex-1", "flex flex-col", "gap-2", "overflow-auto", "pl-bg-g")}>
+          <PlFilters class={tw(SCROLL, "z-10", "px-2 pt-4 pb-0", "max-h-[40%]", "bg-linear-to-b from-black to-transparent")} />
+          <PlGrid pg={pg} class={tw(SCROLL, "p-4", "flex-1")} />
         </div>
 
         <div class={tw(SCROLL, "lg:w-[33dvw] lg:max-w-[40rem]", "max-h-[20dvh] sm:max-h-[35dvh] lg:max-h-[unset]")}>
