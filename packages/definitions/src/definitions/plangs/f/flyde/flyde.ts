3832fa9971e37e5da66007a769a26c8fedab3b30
Emmanuel Oga <contact@github.emmanueloga.com>
2024-10-09 06:53:21 -0700

    add flyde

---
+++ packages/definitions/src/definitions/plangs/f/flyde/flyde.ts
@@ -0,0 +1,16 @@
+import type { PlangsGraph } from "@plangs/plangs";
+
+export function define(g: PlangsGraph) {
+  g.nodes.pl
+    .set("pl+flyde", {
+      name: "Flyde",
+      description:
+        "Flyde is a visual programming language designed to build and manage complex workflows easily. It is aimed at enhancing the development process by utilizing a visual interface for coding, assisting users in strategy development, logic sequencing, and overall workflow optimization.",
+      keywords: ["flyde", "visual", "workflow"],
+      websites: [{ title: "Flyde", href: "https://www.flyde.dev/", kind: "homepage" }],
+      images: [{ kind: "logo", title: "Flyde Logo", url: "https://www.flyde.dev/logo.png", width: 512, height: 512 }],
+    })
+    .addParadigms(["paradigm+declarative", "paradigm+visual", "paradigm+workflow"])
+    .addPlatforms(["plat+web"])
+    .addTags(["tag+automation", "tag+low-code", "tag+workflow"]);
+}
