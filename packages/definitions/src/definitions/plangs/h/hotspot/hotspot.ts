347da44169bf2f4c619a8c3f74e72cb7a753a30a
Emmanuel Oga <contact@github.emmanueloga.com>
2024-10-09 12:15:48 -0700

    java logos

--- packages/definitions/src/definitions/plangs/h/hotspot/hotspot.ts
+++ packages/definitions/src/definitions/plangs/h/hotspot/hotspot.ts
@@ -3,7 +3,7 @@ import type { PlangsGraph } from "@plangs/plangs";
 export function define(g: PlangsGraph) {
   g.nodes.pl
     .set("pl+hotspot", {
-      name: "Java HotSpot Virtual Machine",
+      name: "HotSpot VM",
       description:
         "HotSpot, released as Java HotSpot Performance Engine, is a Java virtual machine for desktop and server computers, developed by Sun Microsystems which was purchased by and became a division of Oracle Corporation in 2010. Its features improved performance via methods such as just-in-time compilation and adaptive optimization. It is the de facto Java Virtual Machine, serving as the reference implementation of the Java programming language.",
       keywords: ["hotspot", "jvm"],
