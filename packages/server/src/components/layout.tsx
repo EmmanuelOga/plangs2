383a307a8796186d39519b048bf6c1498900b8df
Emmanuel Oga <contact@github.emmanueloga.com>
2024-10-14 04:44:53 -0700

    change favicon

--- packages/server/src/components/layout.tsx
+++ packages/server/src/components/layout.tsx
@@ -31,10 +31,10 @@ export function Layout({ title, description, tab, pl, mainClasses, children }: L
 
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="stylesheet" href="/bundle/app.css" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link type="image/png" rel="apple-touch-icon" sizes="192x192" href="/icons/icon-192x192.png" />
+        <link type="image/png" rel="icon" sizes="192x192" href="/icons/icon-192x192.png" />
+        <link type="image/png" rel="icon" sizes="72x72" href="/icons/favicon-72x72.png" />
+        <link rel="manifest" href="/manifest.json" />
       </head>
       <body
         data-tab={tab}
