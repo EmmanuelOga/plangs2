cbd824b7e936b35d7b4bff2f2db9f8a9343955cd
Emmanuel Oga <contact@github.emmanueloga.com>
2024-10-15 15:46:15 -0700

    feat: Add lorem ipsum text to facet headers for improved layout

--- packages/server/src/facets/pl.tsx
+++ packages/server/src/facets/pl.tsx
@@ -2,6 +2,7 @@ import { BAR, BORDER } from "@plangs/frontend/styles";
 import { script, tw } from "@plangs/frontend/utils";
 
 import { cl, id } from "../elements";
+import { loremIpsum } from "../utils/lorem";
 import { FacetInput } from "./facet-input";
 import { PL_FACET_GROUPS } from "./pl_config";
 
@@ -14,11 +15,11 @@ export function PlFacets({ class: cssClass }: { class?: string }) {
       class={tw("hidden", "flex flex-row", "overflow-hidden", tw(BORDER, "border-b-1", "border-t-1", "sm:border-r-1"), cssClass)}>
       {script("window.restoreFilters();")}
 
-      {/* Wrapper to avoid streteching the links to the bottom of the screen unnecesarily. */}
-      <div class={tw(tw(BORDER, "border-r-1"), "overflow-y-scroll")}>
+      {/* Wrapper to avoid streteching the links to the bottom of the screen. */}
+      <div class={tw(tw(BORDER, "border-r-1"), "overflow-y-scroll", "grow-0")}>
         <div class={tw("grid grid-cols-[auto_auto]", "gap-2", "pt-1")}>
           {groups.flatMap(groups => (
-            // The subgrid respects the alignment of indicators while allowing to group the links and add a border.
+            // Subgrid respects the alignment of indicators while allowing to group the links and add a border.
             <div key={groups} class={tw("col-span-2", "grid grid-cols-subgrid", "pb-2", tw(BORDER, "border-b-1"))}>
               {groups.map(({ title, key }) => (
                 <FacetLink key={key} facetKey={key} title={title} href={`javascript:window.focusFilter('${key}')`} />
@@ -28,10 +29,13 @@ export function PlFacets({ class: cssClass }: { class?: string }) {
         </div>
       </div>
 
-      <div class={tw("flex-1", "bg-linear-to-b to-secondary/50", "flex flex-col")}>
+      <div class={tw("max-w-full flex-1", "bg-linear-to-b to-secondary/50", "flex flex-col")}>
         {groups.flat(1).map(({ key, title, keys }) => (
           <div key={key} id={key} class={tw(cl("facet"), "hidden", "flex-1", "flex flex-col")}>
-            <header class={tw(BAR, "p-2", "text-primary", tw(BORDER, "border-b-1"))}>{title}</header>
+            <header class={tw(BAR, "p-2", "flex flex-row", "gap-2", "text-primary", "truncate", tw(BORDER, "border-b-1"))}>
+              <span>{title}</span>
+              <span>X Y Z {loremIpsum(100)}</span>
+            </header>
             <div class={tw("flex-1", "flex flex-col", "gap-4")}>
               {keys.map(key => (
                 <FacetInput key={key} inputKey={key} />
