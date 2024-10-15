3ba5fb622a008989e91fd3d8aea4faf43a85eb24
Emmanuel Oga <contact@github.emmanueloga.com>
2024-10-15 08:41:14 -0700

    refactor facet link ;

--- packages/server/src/facets/pl.tsx
+++ packages/server/src/facets/pl.tsx
@@ -9,19 +9,11 @@ export function PlFacets({ class: cssClass }: { class?: string }) {
   const groups = PL_FACET_GROUPS;
 
   const facetLinks = (
-    <div class={tw("mr-1", "grid grid-cols-[auto_auto]", "gap-2", tw(BORDER, "border-r-1"))}>
+    <div class={tw("grid grid-cols-[auto_auto]", "gap-2", tw(BORDER, "border-r-1"))}>
       {groups.flatMap(groups => (
         <div key={groups} class={tw("col-span-2", "grid grid-cols-subgrid", "pb-4", tw(BORDER, "border-b-1"))}>
           {groups.map(({ title, key }) => (
-            <div key={key} class={tw("col-span-2", "grid grid-cols-subgrid")}>
-              <div class={tw(cl("facetIndicator"), "mt-[.45rem] pl-2", "text-primary text-xs")}>⬤</div>
-              <a
-                data-facet={key}
-                href={`javascript:window.focusFilter('${key}')`}
-                class={tw(cl("facetLink"), "block", "truncate", "py-1 pr-2", "text-foreground/85", "underline decoration-1 decoration-dotted")}>
-                {title}
-              </a>
-            </div>
+            <FacetLink key={key} facetKey={key} title={title} href={`javascript:window.focusFilter('${key}')`} />
           ))}
         </div>
       ))}
@@ -41,7 +33,7 @@ export function PlFacets({ class: cssClass }: { class?: string }) {
       {script("window.restoreFilters();")}
 
       <div class={tw("col-span-2", "grid grid-cols-subgrid", tw(BORDER, "border-b-1"), BAR)}>
-        <header class="p-2">Links</header>
+        <header class="p-2">[CLEAR]</header>
         <header class="p-2" id={id("currentFacet")}>
           Facet
         </header>
@@ -60,5 +52,19 @@ export function PlFacets({ class: cssClass }: { class?: string }) {
 
       {script(`setTimeout(() => window.focusFilter('general'), 100);`)}
     </aside>
+  );
+}
+
+function FacetLink({ facetKey, title, href }: { facetKey: string; title: string; href: string }) {
+  return (
+    <>
+      <div class={tw(cl("facetIndicator"), "mt-[.45rem] pl-2", "text-primary text-xs")}>⬤</div>
+      <a
+        href={href}
+        data-facet={facetKey}
+        class={tw(cl("facetLink"), "block", "truncate", "py-1 pr-2", "text-foreground/85", "underline decoration-1 decoration-dotted")}>
+        {title}
+      </a>
+    </>
   );
 }
