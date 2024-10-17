1a79b9aae17e54345dd684d74c03337014d17d91
Emmanuel Oga <contact@github.emmanueloga.com>
2024-10-17 06:15:19 -0700

    added jank :-)

---
+++ packages/definitions/src/definitions/plangs/j/jank/jank.ts
@@ -0,0 +1,23 @@
+import type { PlangsGraph } from "@plangs/plangs";
+
+export function define(g: PlangsGraph) {
+  g.nodes.pl
+    .set("pl+jank", {
+      name: "Jank",
+      description:
+        "Jank is a general-purpose programming language which embraces the interactive, value-oriented nature of Clojure as well as the desire for native compilation and minimal runtimes. jank is strongly compatible with Clojure and considers itself a dialect of Clojure. Please note that jank is under heavy development; assume all features are planned or incomplete.",
+      keywords: ["jank"],
+      websites: [
+        { title: "Jank Official Website", href: "https://jank-lang.org/", kind: "homepage" },
+        { title: "Jank on GitHub", href: "https://github.com/jank-lang/jank", kind: "repository" },
+      ],
+      extensions: [".jank"],
+      firstAppeared: "2015-01-24",
+      isTranspiler: false,
+    })
+    .addInfluencedBy(["pl+clojure"])
+    .addDialectOf(["pl+clojure"])
+    .addParadigms(["paradigm+functional"])
+    .addPlatforms(["plat+cross"])
+    .addWrittenIn(["pl+llvm", "pl+c++"]);
+}
