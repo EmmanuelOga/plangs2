c1035c784311fdf190d074b49ccb87debf21c174
Emmanuel Oga <contact@github.emmanueloga.com>
2024-11-06 00:14:06 -0800

    exporting github-linguist metadata to a single json file

---
+++ archived/linguist/lib/main.rb
@@ -0,0 +1,39 @@
+require 'yajl'
+require 'linguist'
+
+def get_lang_data
+  Linguist::Language.all.map do |lang|
+    group, data = lang.group, {}
+
+    data['name'] = lang.name
+    data['nameFS'] = lang.fs_name if lang.fs_name && lang.fs_name != lang.name
+    data['groupName'] = group.name if group && group.name != lang.name
+    data['aliases'] = lang.aliases || []
+    data['defaultAlias'] = lang.default_alias
+    data['githubLangId'] = lang.language_id
+
+    data['color'] = lang.color
+    data['extensions'] = lang.extensions
+    data['filenames'] = lang.filenames
+    data['interpreters'] = lang.interpreters
+    data['githubPopular'] = !!lang.popular?
+    data['type'] = lang.type
+    data['wrap'] = lang.wrap
+
+    data['aceMode'] = lang.ace_mode
+    data['codemirrorMIME'] = lang.codemirror_mime_type
+    data['codemirrorMode'] = lang.codemirror_mode
+    data['textmateScope'] = lang.tm_scope
+
+    # Remove nil or empty values.
+    data.delete_if do |_, value|
+      value.nil? ||
+      (value.is_a?(String) && value.strip() == '') ||
+      (value.respond_to?(:empty?) && value.empty?)
+    end
+
+    data
+  end
+end
+
+Yajl::Encoder.encode(get_lang_data(), $stdout)
---
+++ archived/linguist/out/linguist.ts
@@ -0,0 +1,78 @@
+/**
+ * Type of the objects generated from github-linguist ruby gem.
+ */
+interface Linguist {
+  /** Language Name. */
+  name: string;
+
+  /**
+   * Language Name in the file system.
+   * For instance, "F*" needs a file system name since "*" is not a valid filename char.
+   */
+  nameFS?: string;
+
+  /**
+   * A group name is more general than a language name.
+   *
+   * Examples:
+   * - name: "Apollo Guidance Computer" group: "Assembly"
+   * - name: "Glimmer JS" group: "JavaScript"
+   * - name: "Glimmer TS" group: "TypeScript"
+   * - name: "Gradle Kotlin DSL" group: "Gradle"
+   * - name: "HTML+PHP" group: "HTML"
+   * - name: "JSON with Comments" group: "JSON"
+   * - name: "Java Server Pages" group: "Java"
+   * - name: "Literate Haskell" group: "Haskell"
+   *
+   * In general Plangs does not want to be super granular,
+   * so we want to work with the group name when there's one.
+   */
+  groupName?: string;
+
+  /**
+   * Name Aliases, if any.
+   *
+   * Examples:
+   * - name: "Protocol Buffer": aliases: ["protocol-buffer","proto","protobuf","Protocol Buffers"]
+   * - name: "Python" aliases: ["python","python3","rusthon"]
+   * - name: "Ragel" aliases: ["ragel","ragel-rb","ragel-ruby"]
+   * - name: "Raku" aliases: ["raku","perl6","perl-6"]
+   */
+  aliases: string[];
+
+  defaultAlias?: string;
+
+  /**
+   * Internal ID of the language in Github.
+   * Looks like a numeric id ("1", "2", "3") wrap in a string.
+   */
+  githubLangId?: string;
+
+  //--------------------------------------------------------------------------------
+
+  /** An HTML color name that Github assigns to some languages. Example: "#2A6277". */
+  color?: string;
+  /** File Extensions, including the dot. Example: [".pas", ".tpu"]. */
+  extensions: string[];
+  /** File names are names that are associated with an specific language. Example: ['Makefile']. */
+  filenames: string[];
+  /** Name of binary executable file that can take a program of this type as input. Example: ['bash', 'dash', 'sh']. */
+  interpreters: string[];
+  /** Languages that Github considers "popular", for whatever reason. */
+  githubPopular?: boolean;
+  /** Language Type */
+  type: "data" | "markup" | "programming" | "prose";
+  /** Should lines be wrapped? According to Github, most language lines should not be wrapped. */
+  wrap?: boolean;
+
+  //--------------------------------------------------------------------------------
+
+  /** Mode for the [ACE editor](https://ace.c9.io). */
+  aceMode: string;
+  /** MIME type used by [CodeMirror](https://codemirror.net/). */
+  codemirrorMIME: string;
+  /** Mode used by [CodeMirror](https://codemirror.net/). */
+  codemirrorMode: string;
+  /** Scope used by [TextMate](https://macromates.com/). */
+  textmateScope: string;
+}
