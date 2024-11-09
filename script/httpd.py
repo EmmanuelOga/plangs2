435be15a662e6140272977cb3e51736d329f8738
Emmanuel Oga <contact@github.emmanueloga.com>
2024-11-09 01:22:43 -0800

    add a simple script to serve the fully static site

---
+++ script/httpd.py
@@ -0,0 +1,19 @@
+## Simple HTTPD server that serves files with .html extension by default
+## Usage: python3 httpd.py
+## Open browser and navigate to http://localhost:8000
+## Used to test the final output of the static site.
+
+import http.server
+import socketserver
+
+# Serve files with .html extension by default
+class CustomRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path.find('.') == -1 and not self.path.endswith('.html'):
+            self.path = f"{self.path}.html"
+        return super().do_GET()
+
+PORT = 8000
+with socketserver.TCPServer(("", PORT), CustomRequestHandler) as httpd:
+    print(f"Serving on port {PORT}")
+    httpd.serve_forever()
