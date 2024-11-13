ef7eceda09b86184f78585bc0b3605dc09ee0c8f
Emmanuel Oga <contact@github.emmanueloga.com>
2024-11-13 02:19:02 -0800

    add to openai prompt

--- packages/ai/src/index.ts
+++ packages/ai/src/index.ts
@@ -66,6 +66,9 @@ async function plangPrompt(pg: PlangsGraph, pl: NPlang, examplePl: NPlang): Prom
       content: [
         "You are an expert at programming languages and structured data extraction. ",
         "You will be given unstructured text from websites about a programming language, and should convert it into the given structure.",
+        "If you don't find a good match for a field, you can leave it empty. ",
+        "For instance, you can live the field compileTo as an empty array if the language is not a transpiler.",
+        "Please leave the images field empty, as we will handle that separately.",
       ].join("."),
     },
     {
