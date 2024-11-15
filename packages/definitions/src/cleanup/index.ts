c7dd2266c2ab7f6945e8936cc4da45d47113a607
Emmanuel Oga <contact@github.emmanueloga.com>
2024-11-14 22:24:49 -0800

    cleanup WIP 1/2 remove websites, use flatter external fields

--- packages/definitions/src/cleanup/index.ts
+++ packages/definitions/src/cleanup/index.ts
@@ -1,55 +1,23 @@
+/*
+ * Perform a "cleanup" of the Plangs! data:
+ * * Add Github linguist data
+ * * Add Languish data including external links to Wikipedia, Reddit and StackOVerflow.
+ * * Home for other cleanup tasks that may need to be performed just once.
+ */
+
 import { arrayMerge } from "@plangs/auxiliar/array";
 import { loadAllDefinitions } from "@plangs/definitions";
 import { plangCodeGen, tspath } from "@plangs/languist/codegen";
-import { LG_LANGS } from "@plangs/languist/languish";
+import { LG_LANGS, type Rankings } from "@plangs/languist/languish";
 import { GH_LANGS } from "@plangs/languist/linguist";
 import type { LanguishKeys, LinguistLang } from "@plangs/languist/types";
 import { type NPlang, PlangsGraph } from "@plangs/plangs";
-
-// Load all Plangs! definitions.
-const pg = new PlangsGraph();
-await loadAllDefinitions(pg, { scanImages: false });
-
-// Load all Languish data.
-const GH_MAP = GH_LANGS.lookupMap();
-
-// Use Languish data to get the rankings for the languages.
-// We use the 2024Q3 data, as it's the most recent one.
-// This should be updated every quarter.
-const RANKINGS: Map<string, number> = LG_LANGS.rankingsMap("2024Q3");
-const REV_RANKINGS: Map<number, string> = new Map(RANKINGS.entries().map(([k, v]) => [v, k]));
-
-/** Find the best match from all GH langs to the given NPlang. */
-function ghBestMatch(pl: NPlang): LinguistLang | undefined {
-  const find = (name: string) => {
-    const clean = name.trim().toLowerCase();
-    if (GH_MAP.has(clean)) return GH_MAP.get(clean); // Easy win.
-
-    // This is a bit inefficient, but since the map is small, it's not a big deal.
-    // We'll find substring matches for languages with more than N characters.
-    if (clean.length > 4) {
-      const candidates: [string, LinguistLang][] = [];
-      for (const [key, lang] of GH_MAP) {
-        if (key.includes(clean)) candidates.push([key, lang]);
-      }
-      // Find the candidate closer in string length.
-      candidates.sort(([key1], [key2]) => Math.abs(clean.length - key1.length) - Math.abs(clean.length - key2.length));
-      if (candidates.length) return candidates[0][1];
-    }
-  };
-
-  // We want to find the best match, for the language itself first, or any of its family.
-  // NOTE: using the family can lead to many false matches.
-  for (const candidate of [pl /*, ...pl.family() */]) {
-    const ghPl = find(candidate.name);
-    if (ghPl) return ghPl;
-  }
-}
+import type { Link } from "@plangs/plangs/schema";
 
 /** Update the NPlang data with Github data. */
-function updateWithGH(pl: NPlang, plGH: LinguistLang) {
-  // Add partial GH data to the Plang node.
-  const { name, groupName, langId, color, popular, type } = plGH;
+function updateWithGH(pl: NPlang, ghMap: Map<string, LinguistLang>): boolean {
+  const plGH = GH_LANGS.bestMatch(ghMap, pl.data.githubName) ?? GH_LANGS.bestMatch(ghMap, pl.name);
+  if (!plGH) return false;
 
   // We used to have nested data and switched to flat data.
   // biome-ignore lint/performance/noDelete: it is ok here.
@@ -61,80 +29,112 @@ function updateWithGH(pl: NPlang, plGH: LinguistLang) {
   pl.data.githubColor = plGH.color;
   pl.data.githubPopular = plGH.popular;
   pl.data.githubType = plGH.type;
+
+  return true;
 }
 
 /** Update the NPlang data with Languish data. */
-function updateWithLanguish(pl: NPlang, plLG: LanguishKeys) {
-  pl.data.languishRanking = RANKINGS.get(plLG[0]);
+function updateWithLanguish(pl: NPlang, rankings: Rankings): boolean {
+  if (!pl.data.githubName) return false;
 
-  // Add partial LG data to the Plang node.
-  if (plLG[2] && !pl.data.websites?.some(w => w.kind === "reddit")) {
-    arrayMerge(
-      (pl.data.websites ??= []),
-      [{ kind: "reddit", title: `${pl.name} on Reddit`, href: `https://reddit.com/r/${plLG[2]}` }],
-      (l1, l2) => l1.href === l2.href,
-    );
-  }
-  if (plLG[1] && !pl.data.websites?.some(w => w.kind === "wikipedia")) {
-    arrayMerge(
-      (pl.data.websites ??= []),
-      [{ kind: "wikipedia", title: `${pl.name} on Wikipedia`, href: `https://en.wikipedia.org/wiki/${plLG[1]}` }],
-      (l1, l2) => l1.href === l2.href,
-    );
-  }
+  const plLG = LG_LANGS.keysByName.get(pl.data.githubName) as LanguishKeys;
+  if (!plLG) return false;
+
+  pl.data.languishRanking = rankings.rankByGHName.get(pl.name);
+
+  // Prefer the Languish data over whatever is already there.
+  if (plLG[1]) pl.data.extWikipediaPath = plLG[1];
+  if (plLG[2]) pl.data.extRedditPath = plLG[2];
   if (plLG[3]) arrayMerge((pl.data.stackovTags ??= []), plLG[3].split("|"));
+
+  return true;
 }
 
-/** Find matching data to add to the Plang nodes, then recreate the code definition to include this data. */
-export function mergeGHandLanguishData() {
-  // Find the best match for each Plang node on the GH data.
-  for (const pl of pg.nodes.pl.values) {
-    const plGH = ghBestMatch(pl);
-    if (plGH) updateWithGH(pl, plGH);
-    if (!pl.data.githubName) continue;
-    const plLG = LG_LANGS.keysByName.get(pl.data.githubName);
-    if (plLG) updateWithLanguish(pl, plLG);
-  }
-
+/** Languish has a rank for the top 100 languages of linguist. Create skeleton files for those, if missing. */
+export async function createMissingPlangs(pg: PlangsGraph, ghMap: Map<string, LinguistLang>, rankings: Rankings) {
   // Collect the existing GH names for the next step.
-  const existingGHNames = new Set<string>();
-  for (const pl of pg.nodes.pl.values) {
-    const { githubName } = pl.data;
-    if (githubName) existingGHNames.add(githubName);
-  }
+  const existingGHNames = new Set<string>(pg.nodes.pl.values.map(pl => pl.data.githubName).existing);
 
-  // We want to ensure we have the top 100 languages in the rankings.
-  for (let i = 1; i <= 100; i++) {
-    const key = REV_RANKINGS.get(i);
-    if (!key) throw new Error(`Missing key for ranking ${i}. This should never happen!`);
+  // Find languages to create new Plang nodes for (for languages with GH ranking from 1 to 100).
+  // After this we need to manually review the results: some languages may not be worth keeping (like "AdBlock lists" format).
+  for (let rank = 1; rank <= 100; rank++) {
+    const ghName = rankings.ghNameByRank.get(rank);
+    if (!ghName) throw new Error(`Missing key for ranking ${rank}. This should never happen!`);
 
     // If the key is already in the map, we are done.
-    if (existingGHNames.has(key)) continue;
+    if (existingGHNames.has(ghName)) continue;
 
     // If the key is not in the map, we need to add a new Plang node.
-    const pl = pg.nodes.pl.set(`pl+${key.toLowerCase().replaceAll(/\s+/g, "-")}`, { name: key, languishRanking: i });
+    const pl = pg.nodes.pl.set(`pl+${ghName.toLowerCase().replaceAll(/\s+/g, "-")}`, { name: ghName, languishRanking: rank });
 
-    const plGH = GH_MAP.get(key);
-    if (plGH) {
-      updateWithGH(pl, plGH);
-    } else {
-      console.warn(`Missing GH data for ${key}.`);
-    }
-
-    const plLG = LG_LANGS.keysByName.get(key);
-    if (plLG) {
-      updateWithLanguish(pl, plLG);
-    } else {
-      console.warn(`Missing Languish data for ${key}.`);
-    }
-  }
-
-  // Re-generate all the language files.
-  for (const [key, pl] of pg.nodes.pl) {
-    Bun.write(tspath(pl.plainKey), plangCodeGen(pl));
+    updateWithGH(pl, ghMap);
+    updateWithLanguish(pl, rankings);
   }
 }
 
-mergeGHandLanguishData();
+export async function cleanupData() {
+  const pg = new PlangsGraph();
+  await loadAllDefinitions(pg, { scanImages: false });
 
+  // These are expensive so create them once before starting.
+  const ghMap = GH_LANGS.lookupMap();
+  const rankings = LG_LANGS.rankings();
+
+  const woGH = new Set<string>();
+  const woLG = new Set<string>();
+
+  // Find the best match for each Plang node on the GH data.
+  for (const pl of pg.nodes.pl.values) {
+    if (updateWithGH(pl, ghMap)) {
+      // Languish works with github name, so it can only work if the GH data is there.
+      if (!updateWithLanguish(pl, rankings)) woLG.add(pl.name);
+    } else woGH.add(pl.name);
+  }
+
+  console.warn("Languages without updates:", { woGithub: [...woGH], woLanguish: [...woLG] });
+
+  // TEMP: remove this after the data is fixed.
+  function removeWebsites(pl: NPlang, matcher: (link: Link) => boolean) {
+    const before = pl.websites.existing;
+    const after = pl.websites.filter(matcher);
+    if (before.length !== after.length) pl.data.websites = after;
+  }
+
+  // Places the github path on the data for easy access.
+  for (const pl of pg.nodes.pl.values) {
+    removeWebsites(pl, ({ href }) => {
+      const url = new URL(href);
+      if (url.hostname === "github.com") {
+        if (!pl.data.extGithubPath) {
+          const match = url.pathname.match(/^\/([^/]+)\/([^/]+)$/);
+          if (match) pl.data.extGithubPath = `${match[1]}/${match[2]}`;
+        }
+        return false;
+      }
+      if (url.hostname === "en.wikipedia.org") {
+        if (!pl.data.extWikipediaPath) pl.data.extWikipediaPath = url.pathname.replace(/^\/wiki\//, "");
+        return false;
+      }
+      if (url.hostname.endsWith("reddit.com")) {
+        if (!pl.data.extRedditPath) pl.data.extRedditPath = url.pathname.replace(/^\/r\//, "");
+        return false;
+      }
+      return true; // Keep it.
+    });
+
+    if (pl.websites.size > 0) {
+      const [first, ...rest] = pl.websites;
+      pl.data.extHomeURL = first.href;
+      pl.data.websites = rest;
+    }
+
+    if (!pl.websites.isEmpty) console.log(pl.name, "remaining websites", pl.websites.map(w => w.href).existing);
+  }
+
+  // await createMissingPlangs(pg, ghMap, rankings);
+
+  for (const pl of pg.nodes.pl.values) Bun.write(tspath(pl.plainKey), plangCodeGen(pl));
+}
+
+await cleanupData();
 console.warn("CAUTION: matching github data is not a perfect process. Results (git diff) should be manually reviewed.");
