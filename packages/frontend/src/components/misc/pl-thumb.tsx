8198c02c41ade91eb8fb0b2305c2e3898fec4bf3
Emmanuel Oga <contact@github.emmanueloga.com>
2024-11-18 18:18:29 -0800

    add thumb

--- packages/frontend/src/components/misc/pl-thumb.tsx
+++ packages/frontend/src/components/misc/pl-thumb.tsx
@@ -12,9 +12,22 @@ export function PlThumb({ pl, class: klass }: { class?: string; pl: NPlang }): J
     <div
       data-node-key={pl.key}
       // Use a high ranking for those that don't have one, so they appear at the end when sorted.
-      data-node-ranking={`${pl.ranking ?? 999_999}`}
+      data-node-ranking={`${pl.ranking ?? ""}`}
       class={tw(cssClass("plThumb"), "group relative", "cursor-pointer", "max-w-[7rem] sm:max-w-[15rem]", "max-h-[7rem] sm:max-h-[15rem]", klass)}>
       <div class={tw("truncate text-center", "text-foreground group-hover:text-primary")}>{pl.name}</div>
+      {pl.ranking && (
+        <div
+          title={`Languish Ranking: ${pl.ranking}`}
+          class={tw(
+            // ALlow hiding the ranking using a data attribute on the wrapper.
+            "group-[[data-hide-ranking='1']]:hidden",
+            "absolute right-0 bottom-0",
+            "px-1 text-primary text-xs",
+            "opacity-50",
+          )}>
+          <a href={`https://tjpalmer.github.io/languish/#names=${encodeURIComponent(pl.name.toLowerCase())}`}>{pl.ranking}</a>
+        </div>
+      )}
       <div
         class={tw(
           "p-4",
@@ -26,19 +39,6 @@ export function PlThumb({ pl, class: klass }: { class?: string; pl: NPlang }): J
         )}>
         <img alt={`${pl.name}'s logo`} class="max-h-full max-w-full object-contain" src={thumbUrl ?? PLACEHOLDER} />
       </div>
-      {pl.ranking && (
-        <div
-          title={`Languish Ranking: ${pl.ranking}`}
-          class={tw(
-            // ALlow hiding the ranking using a data attribute on the wrapper.
-            "group-[[data-hide-ranking='1']]:hidden",
-            "absolute right-1 bottom-1",
-            "rounded bg-background bg-opacity-50",
-            "p-1 text-primary text-xs",
-          )}>
-          <a href={`https://tjpalmer.github.io/languish/#names=${encodeURIComponent(pl.name.toLowerCase())}`}>#{pl.ranking}</a>
-        </div>
-      )}
     </div>
   );
 }
