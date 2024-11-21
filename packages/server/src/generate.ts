05cbfed3bd82be0d58b70a83a5756b532a55012d
Emmanuel Oga <contact@github.emmanueloga.com>
2024-11-21 03:41:27 -0800

    restore the ability to generate the json data

--- packages/server/src/generate.ts
+++ packages/server/src/generate.ts
@@ -68,10 +68,20 @@ async function generatePages(dstRoot: string) {
   }
 }
 
+async function generateJSON(dstRoot: string) {
+  const pg = new PlangsGraph();
+  await loadAllDefinitions(pg, { scanImages: false });
+  Bun.write(join(dstRoot, "plangs.json"), JSON.stringify(pg));
+}
+
 if (process.argv[2] === "generate") {
   const dstRoot = process.argv[3];
   console.log("Generating to", dstRoot);
   await generatePages(dstRoot);
+} else if (process.argv[2] === "json") {
+  const dstRoot = process.argv[3];
+  console.log("Generating to", dstRoot);
+  await generateJSON(dstRoot);
 } else {
   console.log("Usage: cmd generate dst-root-path");
 }
