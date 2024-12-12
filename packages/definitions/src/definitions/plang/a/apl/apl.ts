9f3f68ed284bb7151dda42ca7eee154332be81ee
Emmanuel Oga <contact@github.emmanueloga.com>
2024-12-11 17:16:05 -0800

    tweak def

--- packages/definitions/src/definitions/plang/a/apl/apl.ts
+++ packages/definitions/src/definitions/plang/a/apl/apl.ts
@@ -5,7 +5,7 @@ export function define(g: PlangsGraph) {
     .set("pl+apl", {
       name: "APL",
       description:
-        "APL (named after the book A Programming Language) is a programming language developed in the 1960s by Kenneth E. Iverson. Its central datatype is the multidimensional array. It uses a large range of special graphic symbols to represent most functions and operators, leading to very concise code. It has been an important influence on the development of concept modeling, spreadsheets, functional programming, and computer math packages. It has also inspired several other programming languages.",
+        "APL (named after the book A Programming Language) is a programming language developed in the 1960s by Kenneth E. Iverson. Its central datatype is the multi-dimensional array. It uses a large range of special graphic symbols to represent most functions and operators, leading to very concise code. It has been an important influence on the development of concept modeling, spreadsheets, functional programming, and computer math packages. It has also inspired several other programming languages.",
       keywords: ["apl", "array programming"],
       extensions: [".apl"],
       releases: [
