52a6dadf8b081a2540b3cf65e3713a5efdaf2ca8
Emmanuel Oga <contact@github.emmanueloga.com>
2024-12-13 04:18:25 -0800

    generate a graphviz diagram for docs

--- packages/server/src/generators/graph.ts
+++ packages/server/src/generators/graph.ts
@@ -2,5 +2,6 @@ import { generateGraph } from "@plangs/graphgen/index";
 import { PLANGS_GRAPH_SPEC } from "@plangs/plangs/graph/spec";
 
 const dst = "packages/plangs/src/graph/generated.ts";
+const diagram = "packages/plangs/src/graph/diagram.dot";
 console.log("Generating graph to", dst, new Date().toISOString());
-await generateGraph(PLANGS_GRAPH_SPEC, dst);
+await generateGraph(PLANGS_GRAPH_SPEC, dst, diagram);
