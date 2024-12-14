d033982c38cb0b07acb72821bd632ced19ac8e35
Emmanuel Oga <contact@github.emmanueloga.com>
2024-12-13 18:22:23 -0800

    graph analysis refactor

--- packages/server/src/generators/graph.ts
+++ packages/server/src/generators/graph.ts
@@ -1,7 +1,16 @@
-import { generateGraph } from "@plangs/graphgen/index";
+import { analyzeGraph, generateGraph } from "@plangs/graphgen/index";
 import { PLANGS_GRAPH_SPEC } from "@plangs/plangs/graph/spec";
 
-const dst = "packages/plangs/src/graph/generated.ts";
-const diagram = "packages/plangs/doc";
-console.log("Generating graph to", dst, new Date().toISOString());
-await generateGraph(PLANGS_GRAPH_SPEC, dst, diagram);
+const argv = process.argv[2] ?? "";
+
+if (argv === "") {
+  const dst = "packages/plangs/src/graph/generated.ts";
+  console.log("Generating graph to", dst, new Date().toISOString());
+  await generateGraph(PLANGS_GRAPH_SPEC, dst);
+} else if (argv === "analysis") {
+  console.log("Analyzing graph");
+  const diagram = "packages/plangs/doc";
+  analyzeGraph(PLANGS_GRAPH_SPEC, diagram);
+} else {
+  console.error("Invalid argument:", argv);
+}
