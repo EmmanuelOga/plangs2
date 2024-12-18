70479f766b193ef56ff8ea0209088438a8856f24
Emmanuel Oga <contact@github.emmanueloga.com>
2024-12-17 22:42:13 -0800

    remove keywords from vertex page

--- packages/server/src/pages/vertex.tsx
+++ packages/server/src/pages/vertex.tsx
@@ -11,7 +11,7 @@ import { cssClass } from "../elements";
 const RELATIONS = [
   ["relPosts", "News"],
   ["relTools", "Tools"],
-  ["relBundles", "Bundles"],
+  ["relBundles", "Bundles", "A bundle is a collection of tools that work well together."],
   ["relApps", "Applications"],
   ["relLibraries", "Libraries"],
   ["relPlangs", "Plangs"],
@@ -28,19 +28,19 @@ export function Vertex({ page, vertex }: { pg: PlangsGraph; page: PlangsPage; ve
       desc={`${vertex.name} at Plangs!: A Programming Language is much more than just a syntax and semantics. It's an Ecosystem of Tools, Libraries, Applications, Learning Resources, and more!`}
       mainClasses={tw("overflow-y-scroll")}>
       <article class={tw("p-4", PROSE)}>
-        <div class={tw(cssClass("vertexInfo"))} data-page={page} data-open={false}>
+        <div class={tw(cssClass("vertexInfo"))} data-open={false}>
           <VertexInfo detail={vertex.detail} page={page} open={false} />
         </div>
 
-        {RELATIONS.map(([rel, title]) => (
-          <VertexRelation key={rel} vertex={vertex} rel={rel} title={title} />
+        {RELATIONS.map(([rel, title, desc]) => (
+          <VertexRelation key={rel} vertex={vertex} rel={rel} title={title} desc={desc} />
         ))}
       </article>
     </Layout>
   );
 }
 
-function VertexRelation({ vertex, rel, title }: { vertex: TPlangsVertex; rel: string; title: string }) {
+function VertexRelation({ vertex, rel, title, desc }: { vertex: TPlangsVertex; rel: string; title: string; desc?: string }) {
   type Direct = RelTo<TPlangsVertex, TPlangsVertex>;
   type Indirect = RelFrom<TPlangsVertex, TPlangsVertex>;
   const relation = vertex[rel as keyof typeof vertex] as Direct | Indirect;
@@ -54,20 +54,16 @@ function VertexRelation({ vertex, rel, title }: { vertex: TPlangsVertex; rel: st
   return (
     <div class="mt-4 mb-16">
       <h2 class="mt-0!">{title}</h2>
+      {desc && <p>{desc}</p>}
       {
-        <Table headers={isNews ? ["Date", "Post"] : ["Name", "Keywords", "Description"]}>
+        <Table headers={isNews ? ["Date", "Post"] : ["Name", "Description"]}>
           {relVertices.map(vertex => (
             <tr key={vertex.key} class={tw("hover:bg-hiliteb/10")}>
               {isNews && <td>{(vertex as VPost).date}</td>}
               <td>
                 <a href={vertex.href} title={vertex.name} children={vertex.name} />
               </td>
-              {!isNews && (
-                <>
-                  <td>{vertex.keywords.join(", ")}</td>
-                  <td>{vertex.shortDesc}</td>
-                </>
-              )}
+              {!isNews && <td>{vertex.shortDesc}</td>}
             </tr>
           ))}
         </Table>
