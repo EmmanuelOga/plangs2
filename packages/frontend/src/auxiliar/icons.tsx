c80382c36990e4921a94f6e0290c4bdbc00e8625
Emmanuel Oga <contact@github.emmanueloga.com>
2024-12-18 05:48:59 -0800

    uniform icon style for reference

--- packages/frontend/src/auxiliar/icons.tsx
+++ packages/frontend/src/auxiliar/icons.tsx
@@ -126,14 +126,6 @@ export const SORT_UP = (
   </svg>
 );
 
-export const EXTERNAL = (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18" class={CSS}>
-    <title>External</title>
-    <path d="M12.5,13h-1a.5.5,0,0,0-.5.5V15H3V3h8V4.5a.5.5,0,0,0,.5.5h1a.5.5,0,0,0,.5-.5v-3a.5.5,0,0,0-.5-.5H1.5a.5.5,0,0,0-.5.5v15a.5.5,0,0,0,.5.5h11a.5.5,0,0,0,.5-.5v-3A.5.5,0,0,0,12.5,13Z" />
-    <path d="M17.9255,8.8275,14.666,5.1a.39352.39352,0,0,0-.2635-.1.4.4,0,0,0-.4.4V8H8.5a.5.5,0,0,0-.5.5v1a.5.5,0,0,0,.5.5H14v2.6a.4.4,0,0,0,.4.4.39352.39352,0,0,0,.2635-.1l3.262-3.7225A.25.25,0,0,0,17.9255,8.8275Z" />
-  </svg>
-);
-
 export const SEARCH = (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18" class={CSS}>
     <title>Search</title>
--- packages/server/src/components/vertex-entry.tsx
+++ packages/server/src/components/vertex-entry.tsx
@@ -1,7 +1,6 @@
-import { BORDER, tw } from "@plangs/frontend/auxiliar/styles";
+import { EXTERN } from "@plangs/frontend/auxiliar/icons";
+import { BORDER, HOVER_ICON, tw } from "@plangs/frontend/auxiliar/styles";
 import type { TPlangsVertex } from "@plangs/plangs/graph/generated";
-
-import { VertexLink } from "./vertex-link";
 
 export function VertexEntry({ vertex }: { vertex: TPlangsVertex }) {
   return (
@@ -10,6 +9,23 @@ export function VertexEntry({ vertex }: { vertex: TPlangsVertex }) {
         <VertexLink vertex={vertex} external={true} />
       </header>
       <p>{vertex.description}</p>
+    </div>
+  );
+}
+
+function VertexLink({ vertex, external, title }: { vertex: TPlangsVertex; external?: boolean; title?: string }) {
+  const label = title ?? vertex.name;
+  const href = external && title && vertex.urlHome ? vertex.urlHome : vertex.href;
+  return (
+    <div class={tw("group", "inline-flex flex-row items-center gap-1 align-middle")}>
+      <a class="text-xl" href={href} title={label}>
+        {label}
+      </a>
+      {external && vertex.urlHome && (
+        <a href={vertex.urlHome} class={tw("inline-block scale-66", "p-2", "rounded opacity-50", "hover:opacity-100", HOVER_ICON)}>
+          {EXTERN}
+        </a>
+      )}
     </div>
   );
 }
