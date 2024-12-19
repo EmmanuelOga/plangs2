491dd9d08d689c6be525a20f8604841a1e73b6c8
Emmanuel Oga <contact@github.emmanueloga.com>
2024-12-18 19:43:56 -0800

    nicer release date display

--- packages/plangs/src/graph/vertex_data_fields.ts
+++ packages/plangs/src/graph/vertex_data_fields.ts
@@ -32,7 +32,7 @@ export class ReleaseWrapper {
 
   get yearMonth() {
     const { year, month } = this;
-    return year && month ? `${year}-${month.toString().padStart(2, "0")}` : year;
+    return year && month ? `${month.toString().padStart(2, "0")}/${year}` : year;
   }
 
   isRecent(referenceDate: StrDate): boolean {
