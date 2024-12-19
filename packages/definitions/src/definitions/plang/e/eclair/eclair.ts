0b5c1e9389789eef63475ff218a9c836e6acb849
Emmanuel Oga <contact@github.emmanueloga.com>
2024-12-18 20:02:53 -0800

    add eclair

---
+++ packages/definitions/src/definitions/plang/e/eclair/eclair.ts
@@ -0,0 +1,32 @@
+import type { PlangsGraph } from "@plangs/plangs/graph";
+
+export function define(g: PlangsGraph) {
+  g.plang
+    .set("pl+eclair", {
+      name: "Eclair",
+      description:
+        "A logic programming language designed for specifying complex rules over large datasets efficiently, particularly used in the development of compilers and analyzing data dependencies.",
+      shortDesc: "A logic programming language for specifying complex rules over large datasets efficiently.",
+      created: "2021-06",
+      extGithubPath: "luc-tielen/eclair-lang",
+      extHomeURL: "https://github.com/luc-tielen/eclair-lang",
+      isTranspiler: false,
+      keywords: ["data analysis", "dependency analysis", "logic programming"],
+      links: [{ url: "https://github.com/luc-tielen/eclair-lang", title: "Eclair on GitHub" }],
+    })
+    .relInfluencedBy.add("pl+souffle")
+    .relLicenses.add("lic+mit")
+    .relParadigms.add("para+declarative", "para+logic")
+    .relPlatforms.add("plat+cross")
+    .relTags.add("tag+analysis", "tag+compiler")
+    .relTypeSystems.add("tsys+static")
+    .relWrittenWith.add("pl+haskell");
+
+  // TOOLS
+
+  // TOOL BUNDLES
+
+  // LIBRARIES
+
+  // APPS
+}
