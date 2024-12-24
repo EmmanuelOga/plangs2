61b8f2d8733aef31783abcb1b31ff02989c2b5a4
Emmanuel Oga <contact@github.emmanueloga.com>
2024-12-24 05:24:43 -0800

    pill tweaks

--- packages/frontend/src/components/misc/pill.tsx
+++ packages/frontend/src/components/misc/pill.tsx
@@ -3,16 +3,15 @@ import type { ComponentChildren } from "preact";
 
 export function Pill({ children, title }: { title?: string; children: ComponentChildren }) {
   return (
-    // shadow-md inset-shadow-sm inset-shadow-white/20 ring ring-blue-600 inset-ring inset-ring-white/15
     <div
       {...(title ? { title } : {})}
       class={tw(
-        "inline-flex items-center",
-        "mr-4 mb-3 h-8 px-2",
-        "border-1 border-foreground/25",
-        "rounded-tl-2xl rounded-br-2xl",
+        "max-w-full",
+        "mr-4 mb-3 px-2",
+        "outline-1 outline-primary/50",
+        "rounded-tl-lg rounded-br-lg",
         "bg-secondary/75 text-foreground/85",
-        "overflow-ellipsis whitespace-nowrap",
+        "truncate",
       )}>
       {children}
     </div>
