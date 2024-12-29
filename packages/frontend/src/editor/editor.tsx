8c10f980ed5d4340714f9f61529625ce157e40c1
Emmanuel Oga <contact@github.emmanueloga.com>
2024-12-29 00:49:46 -0800

    wip editor

--- packages/frontend/src/editor/editor.tsx
+++ packages/frontend/src/editor/editor.tsx
@@ -6,6 +6,7 @@ import { ADD, CLOSE } from "@plangs/frontend/auxiliar/icons";
 import { INPUT, tw } from "@plangs/frontend/auxiliar/styles";
 import { Pill } from "@plangs/frontend/components/misc/pill";
 import type { RelFrom, RelTo } from "@plangs/graphgen/library";
+import { parseDate } from "@plangs/plangs/auxiliar/str_date";
 import { type PlangsGraph, VLicense, VPlang } from "@plangs/plangs/graph";
 import type { TPlangsVertex, TPlangsVertexName } from "@plangs/plangs/graph/generated";
 
@@ -17,6 +18,7 @@ class EditorState extends Dispatchable<{
   currentVertex?: TPlangsVertex;
   currentRel?: [key: string, rel: AnyRel];
   filter: string;
+  form?: VertexForm;
   tab: "form" | "relations" | "json";
 }> {
   doSetCurrentKind(name: TPlangsVertexName) {
@@ -26,6 +28,7 @@ class EditorState extends Dispatchable<{
 
   doSetCurrentVertex(v: TPlangsVertex) {
     this.data.currentVertex = v;
+    this.data.form = new VertexForm(v).reload();
     this.dispatch();
   }
 
@@ -42,6 +45,11 @@ class EditorState extends Dispatchable<{
   doSetRel(rel: [string, AnyRel]): void {
     this.data.currentRel = rel;
     this.dispatch();
+  }
+
+  get form() {
+    if (this.data.currentVertex) this.data.form ??= new VertexForm(this.data.currentVertex).reload();
+    return this.data.form?.component();
   }
 
   get currentKind() {
@@ -85,7 +93,7 @@ export function PlangsEditor({ pg }: { pg: PlangsGraph }) {
         filter: "",
         currentVertex: py,
         currentRel: ["relInfluenced", py.relations.get("relInfluenced") as AnyRel],
-        tab: "relations",
+        tab: "form",
       }),
   );
 
@@ -125,7 +133,6 @@ export function PlangsEditor({ pg }: { pg: PlangsGraph }) {
         </div>
         {state.currentVertex ? tabs(state, state.currentVertex) : <div class="flex-1 bg-secondary/25 p-2 text-2xl">Select a vertex to edit.</div>}
       </div>
-      <footer class="border-primary border-t-1 bg-secondary/25 p-4">Ready.</footer>
     </div>
   );
 }
@@ -150,7 +157,7 @@ function button({ label, isCurrent, onClick }: { label: string; isCurrent?: () =
 
 function tabs(state: EditorState, vertex: TPlangsVertex) {
   return (
-    <div class={tw("flex flex-1 flex-col gap-4", "bg-secondary/10")}>
+    <div class={tw("flex flex-1 flex-col gap-4", "bg-secondary/10", "overflow-hidden")}>
       <div class="flex flex-row gap-4">
         <div class="w-32">{button({ label: "FORM", isCurrent: () => state.tab === "form", onClick: () => state.doSetTab("form") })}</div>
         <div class="w-32">
@@ -161,16 +168,13 @@ function tabs(state: EditorState, vertex: TPlangsVertex) {
       <header class="border-1 border-primary bg-secondary/75 py-2 text-center text-xl">
         {vertex.vertexName}: {vertex.key}: {vertex.name}
       </header>
-      {state.tab === "form" ? (
-        form(vertex)
-      ) : state.tab === "relations" ? (
-        relations(state, vertex)
-      ) : (
-        <div class="grid grid-cols-[auto_1fr] items-start gap-4 pt-4">{textArea("Raw JSON Data", JSON.stringify(vertex.data, null, 2), 80)}</div>
-      )}
+      <div class="flex-1 overflow-hidden overflow-y-scroll">
+        {state.tab === "form" ? state.form : state.tab === "relations" ? relations(state, vertex) : "TODO"}
+      </div>
     </div>
   );
 }
+// <div class="grid grid-cols-[auto_1fr] items-start gap-4 pt-4">{textArea("Raw JSON Data", JSON.stringify(vertex.data, null, 2), 80)}</div>
 
 function relations(state: EditorState, vertex: TPlangsVertex): ComponentChildren {
   return (
@@ -245,57 +249,176 @@ function partition(state: EditorState, [key, rel]: [string, AnyRel]) {
   );
 }
 
-function inputField(label: string, value?: string | boolean | number, kind: "text" | "checkbox" = "text") {
-  const cssid = `field-${label}`;
-  return (
-    <>
-      <label for={cssid}>{label}</label>
-      <div>
-        {kind === "checkbox" && <input id={cssid} type="checkbox" checked={value as boolean} />}
-        {kind === "text" && <input id={cssid} class={tw(INPUT, "w-full")} type="text" value={(value as string) ?? ""} />}
+class VertexForm {
+  fields: Record<string, { kind: "bool" | "text" | "string"; label: string; desc?: string }> = {};
+  data: Record<string, number | string | boolean | undefined> = {};
+  errors: Record<string, string> = {};
+
+  constructor(public readonly vertex: TPlangsVertex) {
+    this.fields = {
+      name: { kind: "string", label: "Name" },
+      description: { kind: "text", label: "Description" },
+      shortDesc: { kind: "text", label: "Short Description" },
+      created: { kind: "string", label: "Created" },
+      keywords: { kind: "string", label: "Keywords", desc: "Comma-separated list of keywords." },
+      urlHome: { kind: "string", label: "Home URL", desc: "URL including protocol (http, https)." },
+      extRedditPath: { kind: "string", label: "Reddit Path", desc: "Path to subreddit. Example: Python for https://www.reddit.com/r/Python/" },
+      extWikipediaPath: {
+        kind: "string",
+        label: "Wikipedia Path",
+        desc: "Path to wikipedia. Example: Python_(programming_language) for https://en.wikipedia.org/wiki/Python_(programming_language)",
+      },
+      stackovTags: { kind: "string", label: "Stack Overflow Tags", desc: "Comma-separated list of tags." },
+    };
+
+    if (vertex instanceof VPlang) {
+      this.fields.extensions = { kind: "string", label: "Extensions", desc: "Comma-separated list of extensions. Example: .py,.pyc" };
+      this.fields.filenames = { kind: "string", label: "Filenames", desc: "Comma-separated list of files associated. Example: Makefile" };
+      this.fields.isTranspiler = { kind: "bool", label: "Is Transpiler", desc: "Compiles to any other language." };
+    }
+
+    if (vertex instanceof VLicense) {
+      this.fields.spdx = { kind: "string", label: "SPDX" };
+      this.fields.isFSFLibre = { kind: "bool", label: "FSF Libre?" };
+      this.fields.isOSIApproved = { kind: "bool", label: "OSI Approved?" };
+    }
+
+    if ("github" in vertex) {
+      this.fields.extGithubPath = { kind: "string", label: "Github Path", desc: "Example: EmmanuelOga/plangs2 for github.com/emmanueloga/plangs2" };
+      this.fields.githubStars = { kind: "string", label: "Github Stars" };
+    }
+
+    this.validate();
+  }
+
+  reload(): this {
+    const vertex = this.vertex;
+
+    this.data = {
+      description: vertex.description,
+      name: vertex.name,
+      shortDesc: vertex.data.shortDesc,
+      created: vertex.created.value,
+      keywords: vertex.keywords.join(","),
+      urlHome: vertex.urlHome,
+      extRedditPath: vertex.data.extRedditPath,
+      extWikipediaPath: vertex.data.extWikipediaPath,
+      stackovTags: vertex.data.stackovTags?.join(","),
+    };
+
+    if (vertex instanceof VPlang) {
+      this.data.extensions = vertex.extensions.join(",");
+      this.data.filenames = vertex.filenames.join(",");
+      this.data.isTranspiler = vertex.isTranspiler;
+    }
+
+    if (vertex instanceof VLicense) {
+      this.data.spdx = vertex.spdx;
+      this.data.isFSFLibre = vertex.isFSFLibre;
+      this.data.isOSIApproved = vertex.isOSIApproved;
+    }
+
+    if ("github" in vertex) {
+      this.data.extGithubPath = vertex.data.extGithubPath;
+      this.data.githubStars = vertex.data.githubStars;
+    }
+
+    return this.validate();
+  }
+
+  validate(): this {
+    const data = this.data;
+    const err: Record<string, string> = (this.errors = {});
+
+    function isNonEmptyStr(attr: string) {
+      const val = `${data[attr] ?? ""}`;
+      if (!val) err[attr] = "Cannot be empty.";
+    }
+
+    function isStrDate(attr: string) {
+      const val = parseDate(`${data[attr] ?? ""}`);
+      if (!val) err[attr] = "Invalid date format. Use YYYY-MM-DD.";
+    }
+
+    function isCSV(attr: string, each: RegExp) {
+      if (!data[attr]) return;
+      const val = `${data[attr]}`.split(",");
+      if (!val.every(v => each.test(v))) err[attr] = `Invalid format. Should match RegExp: ${each.source}`;
+    }
+
+    function isURL(attr: string) {
+      let url: URL | undefined;
+      try {
+        url = new URL(`${data[attr] ?? ""}`);
+      } catch {}
+      if (!url || !url.hostname || !url.protocol) err[attr] = "Invalid URL format.";
+    }
+
+    function matchesRegex(attr: string, rx: RegExp) {
+      if (!rx.test(`${data[attr] ?? ""}`)) err[attr] = `Invalid format. Should match RegExp: ${rx.source}`;
+    }
+
+    function isNumber(attr: string, valid: (n: number) => boolean) {
+      const val = Number.parseInt(`${data[attr] ?? ""}`);
+      if (Number.isNaN(val) || !valid(val)) err[attr] = "Invalid number.";
+    }
+
+    if ("description" in data) isNonEmptyStr("description");
+    if ("name" in data) isNonEmptyStr("name");
+    if ("shortDesc" in data) isNonEmptyStr("shortDesc");
+    if ("created" in data) isStrDate("created");
+    if ("keywords" in data) isCSV("keywords", /[a-zA-Z0-9\-\_]+/);
+    if ("urlHome" in data) isURL("urlHome");
+    if ("extRedditPath" in data) matchesRegex("extRedditPath", /[a-zA-Z0-9_]+$/);
+    if ("extWikipediaPath" in data) matchesRegex("extRedditPath", /[a-zA-Z0-9\_\(\)\-]+$/);
+    if ("stackovTags" in data) isCSV("stackovTags", /[a-zA-Z0-9\-\_]+/);
+    if ("extensions" in data) isCSV("extensions", /^\.[a-zA-Z0-9\-\_]+/);
+    if ("filenames" in data) isCSV("filenames", /[a-zA-Z0-9\-\_]+/);
+    if ("spdx" in data) isNonEmptyStr("spdx");
+    if ("extGithubPath" in data) matchesRegex("extGithubPath", /[a-zA-Z0-9\-\_]+\/[a-zA-Z0-9\-\_]+/);
+    if ("githubStars" in data) isNumber("githubStars", n => n > 0);
+
+    return this;
+  }
+
+  save() {}
+
+  component() {
+    return (
+      <div class="flex min-w-[50%] max-w-[160ch] flex-col gap-4 justify-self-center pr-4">
+        {Object.keys(this.fields).map(attr => this.formField(attr))}
       </div>
-    </>
-  );
-}
+    );
+  }
 
-function textArea(label: string, value?: string, rows = 8) {
-  const cssid = `field-${label}`;
-  return (
-    <>
-      <label for={cssid}>{label}</label>
-      <textarea id={cssid} value={value ?? ""} class={tw(INPUT, "text-xl")} style={`min-height: ${rows}rem; font-family: monospace;`} />
-    </>
-  );
-}
+  formField(attr: string) {
+    if (!(attr in this.fields)) return "Unknown field.";
 
-function form(vertex: TPlangsVertex): ComponentChildren {
-  return (
-    <div class="grid grid-cols-[auto_1fr] items-start gap-4">
-      {inputField("Name", vertex.name)}
-      {textArea("Description", vertex.description)}
-      {textArea("Short Description", vertex.data.shortDesc)}
-      {inputField("Created", vertex.created.value)}
-      {inputField("Keywords", vertex.keywords.join(","))}
-      {inputField("Home URL", vertex.urlHome)}
-      {inputField("Reddit Path", vertex.data.extRedditPath)}
-      {inputField("Wikipedia Path", vertex.data.extWikipediaPath)}
-      {"github" in vertex && inputField("Github Path", vertex.data.extGithubPath)}
-      {"github" in vertex && inputField("Github Stars", vertex.data.githubStars)}
-      {inputField("StackOverflow Tags", vertex.data.stackovTags?.join(","))}
-      {vertex instanceof VPlang && (
-        <>
-          {inputField("Extensions", vertex.extensions.join(","))}
-          {inputField("Filenames", vertex.filenames.join(","))}
-          {inputField("Is Transpiler", vertex.data.stackovTags?.join(","), "checkbox")}
-        </>
-      )}
-      {vertex instanceof VLicense && (
-        <>
-          {inputField("SPDX", vertex.spdx)}
-          {inputField("FSF Libre?", vertex.isFSFLibre, "checkbox")}
-          {inputField("OSI Approved?", vertex.isOSIApproved, "checkbox")}
-        </>
-      )}
-    </div>
-  );
+    const { label, kind, desc } = this.fields[attr];
+    const value = this.data[attr];
+
+    const cssid = `field-${label}`;
+    const error = this.errors[attr];
+    return (
+      <div class="px-4">
+        <label class="block" for={cssid}>
+          {label}
+        </label>
+        {desc && <div class="text-foreground/80 text-sm italic">{desc}</div>}
+        {error && <div class="bg-linear-to-b from-red-200 to-red-300 p-1 text-gray-800">{error}</div>}
+        <div class="py-2">
+          {kind === "bool" && <input id={cssid} type="checkbox" checked={value as boolean} />}
+          {kind === "string" && <input id={cssid} class={tw(INPUT, "w-full")} type="text" value={(value as string) ?? ""} />}
+          {kind === "text" && (
+            <textarea
+              id={cssid}
+              value={value ? `${value}` : ""}
+              class={tw(INPUT, "text-xl", "w-full", "min-h-[10rem]")}
+              style={"font-family: monospace;"}
+            />
+          )}
+        </div>
+      </div>
+    );
+  }
 }
