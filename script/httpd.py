1e876a515e0ec83d7be70f2fb782616d690ff67b
Emmanuel Oga <contact@github.emmanueloga.com>
2024-12-30 05:31:41 -0800

    config tweaks

--- script/httpd.py
+++ script/httpd.py
@@ -6,12 +6,16 @@
 import http.server
 import socketserver
 
+
 # Serve files with .html extension by default
 class CustomRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        if self.path.find('.') == -1 and not self.path.endswith('.html'):
+        if self.path == "/":
+            self.path = "/index.html"
+        elif self.path.find(".") == -1 and not self.path.endswith(".html"):
             self.path = f"{self.path}.html"
         return super().do_GET()
+
 
 PORT = 8000
 with socketserver.TCPServer(("", PORT), CustomRequestHandler) as httpd:
