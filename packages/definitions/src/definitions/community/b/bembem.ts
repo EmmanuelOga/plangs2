9e81a5b49092971c0bb0a41a61204ef18c98e7e2
Emmanuel Oga <contact@github.emmanueloga.com>
2025-01-05 14:53:07 -0800

    Update packages/definitions/src/definitions/community/b/bembem.ts

--- packages/definitions/src/definitions/community/b/bembem.ts
+++ packages/definitions/src/definitions/community/b/bembem.ts
@@ -3,19 +3,21 @@ import type { PlangsGraph } from "@plangs/plangs/graph";
 export function define(g: PlangsGraph) {
   g.community
     .set("comm+bembem", {
-      name: "BEMBEM",
+      name: "BEMBEM!!!!!!!!",
       description:
         "Community by the creator of Plangs! Welcome to Billie English Muffin's Brilliantly Engineered Machine of Boundless Excitement and Marvel! A place for friends to talk about anything. Feel free to share or talk about whatever you may find interesting!",
       extHomeURL: "https://bembem.page/",
       links: [{ title: "Discord", url: "https://discord.gg/MRrkpywtKR" }],
     })
+    .relApps.add("app+aider")
     .relPlangs.add(
       "pl+c",
       "pl+c-sharp",
+      "pl+casette",
+      "pl+chuck",
       "pl+go",
       "pl+javascript",
       "pl+python",
-      "pl+typescript",
     )
     .relTags.add("tag+games", "tag+multimedia", "tag+webdev");
 }
