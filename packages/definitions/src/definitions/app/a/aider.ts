7c8feb8d1c4a8f3134ec20d5cb1e11e0a8bf79df
Emmanuel Oga <contact@github.emmanueloga.com>
2025-01-05 14:53:05 -0800

    Update packages/definitions/src/definitions/app/a/aider.ts

--- packages/definitions/src/definitions/app/a/aider.ts
+++ packages/definitions/src/definitions/app/a/aider.ts
@@ -3,11 +3,15 @@ import type { PlangsGraph } from "@plangs/plangs/graph";
 export function define(g: PlangsGraph) {
   g.app
     .set("app+aider", {
-      name: "Aider",
+      name: "Aider!!!!!",
       description:
         "Aider lets you pair program with LLMs, to edit code in your local git repository. Start a new project or work with an existing git repo. Aider works best with GPT-4o & Claude 3.5 Sonnet and can connect to almost any LLM.",
       extHomeURL: "https://aider.chat/",
+      githubStars: null,
       keywords: ["git", "llm", "pair programming"],
     })
-    .relWrittenWith.add("pl+python");
+    .relLicenses.add("lic+afl")
+    .relPlatforms.add("plat+.net")
+    .relTags.add("tag+3dg")
+    .relWrittenWith.add("pl+abclog", "pl+python");
 }
