6bf097465a6cbdc59b0212ccaed7c5cb99d49231
Emmanuel Oga <contact@github.emmanueloga.com>
2025-01-06 10:04:48 -0800

    Update packages/definitions/src/definitions/plang/p/python.ts

--- packages/definitions/src/definitions/plang/p/python.ts
+++ packages/definitions/src/definitions/plang/p/python.ts
@@ -32,6 +32,7 @@ export function define(g: PlangsGraph) {
       ],
       stackovTags: ["python"],
     })
+    .relCompilesTo.add("pl+javascript")
     .relInfluencedBy.add(
       "pl+ada",
       "pl+apl",
