57e120f8aada363e245598f0a328d2eee24a10d6
Emmanuel Oga <contact@github.emmanueloga.com>
2025-01-08 05:02:39 -0800

    update defintions

--- packages/definitions/src/definitions/plang/j/jsonschema.ts
+++ packages/definitions/src/definitions/plang/j/jsonschema.ts
@@ -2,6 +2,15 @@ import type { PlangsGraph } from "@plangs/plangs/graph";
 
 export function define(g: PlangsGraph) {
   g.plang
-    .set("pl+jsonschema", { name: "JSON Schema" })
+    .set("pl+jsonschema", {
+      name: "JSON Schema",
+      description:
+        "While JSON is probably the most popular format for exchanging data, JSON Schema is the vocabulary that enables JSON data consistency, validity, and interoperability at scale.",
+      shortDesc:
+        "JSON Schema enables the confident and reliable use of the JSON data format.",
+      extGithubPath: "json-schema-org/json-schema-spec",
+      extHomeURL: "https://json-schema.org/",
+      isTranspiler: false,
+    })
     .relTags.add("tag+webdev");
 }
