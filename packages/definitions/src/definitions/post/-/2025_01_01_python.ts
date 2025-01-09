2ec890f555206fb7a2017bea872d6433a59ae825
Emmanuel Oga <contact@github.emmanueloga.com>
2025-01-09 14:19:48 -0800

    Create packages/definitions/src/definitions/post/-/2025_01_01_python.ts

---
+++ packages/definitions/src/definitions/post/-/2025_01_01_python.ts
@@ -0,0 +1,9 @@
+import type { PlangsGraph } from "@plangs/plangs/graph";
+
+export function define(g: PlangsGraph) {
+  g.post.set("post+2025_01_01_python", {
+    name: "Type checking and package management for Python",
+    date: "2025-01-01",
+    path: "2025_01_01_python.md",
+  });
+}
